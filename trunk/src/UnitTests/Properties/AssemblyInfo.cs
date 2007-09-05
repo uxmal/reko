01bc08345e3ce9bc68457872b3f114eac40fc973
johnkal <johnkal@c042da56-5d39-0410-8106-ed1a82c208e8>
2007-09-05 12:02:32 +0000

    * Renamed "Grovel" project to "WindowsGui" for clarity.

--- trunk/src/UnitTests/Properties/AssemblyInfo.cs
+++ trunk/src/UnitTests/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 [assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyFileVersion("0.1.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
