1ea426c387be6bdb606a5443a9c3ff2fe9a61b6c
johnkal <johnkal@c042da56-5d39-0410-8106-ed1a82c208e8>
2007-09-06 11:26:23 +0000

    * BUILD BREAK! Rearrangements needed to allow unit tests.

---
+++ trunk/src/UnitTests/WindowsGui/Controls/MemoryControlTests.cs
@@ -0,0 +1,32 @@
+using Decompiler.WindowsGui.Controls;
+using NUnit.Framework;
+using System;
+
+namespace Decompiler.UnitTests.WindowsGui.Controls
+{
+	[TestFixture]
+	public class MemoryControlTests
+	{
+		public MemoryControlTests()
+		{
+			byte [] bytes = GenerateTestMemory();
+
+		}
+
+		[Test]
+		public void Initialize()
+		{
+			MemoryControl ctl = new MemoryControlTests();
+		}
+
+		private byte [] GenerateTestMemory()
+		{
+			System.IO.MemoryStream stm = new System.IO.MemoryStream();
+			for (int i = 0; i < 1024; ++i)
+			{
+				stm.WriteByte((byte)i);
+			}
+			return stm.ToArray();
+		}
+	}
+}
