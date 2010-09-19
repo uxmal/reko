fb6688090dc3388b83b447109b9dbe7eb144c78f
johnkal <johnkal@c042da56-5d39-0410-8106-ed1a82c208e8>
2010-09-19 09:38:54 +0000

    <UNSTABLE> Moving all Expression subclasses to Expressions folder.

--- trunk/src/Core/Code/Declaration.cs
+++ trunk/src/Core/Code/Declaration.cs
@@ -18,6 +18,7 @@
  */
 #endregion
 
+using Decompiler.Core.Expressions;
 using System;
 
 namespace Decompiler.Core.Code
--- trunk/src/Core/Expressions/ScopeResolution.cs
+++ trunk/src/Core/Expressions/ScopeResolution.cs
@@ -1,3 +1,4 @@
+#region License
 /* 
  * Copyright (C) 1999-2010 John Källén.
  *
@@ -15,6 +16,7 @@
  * along with this program; see the file COPYING.  If not, write to
  * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
  */
+#endregion
 
 using System;
 using Decompiler.Core.Types;
