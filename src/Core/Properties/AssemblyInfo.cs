8133eb59007d9ddff2288a4147899f5435f2f13d
John Källén <uxmal@users.noreply.github.com>
2012-01-02 10:55:04 +0000

    Happy new 2012!

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 #region License
 /* 
- * Copyright (C) 1999-2011 John Källén.
+ * Copyright (C) 1999-2012 John Källén.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -56,7 +56,7 @@ namespace Decompiler
         public const string AssemblyVersion = "0.2.3.*";
         public const string AssemblyFileVersion = "0.2.3.0";
         public const string Product = "Decompiler";
-        public const string Copyright = "Copyright © 1999-2011 John Källén";
+        public const string Copyright = "Copyright © 1999-2012 John Källén";
         public const string Company = "John Källén Konsult AB";
     }
 }
