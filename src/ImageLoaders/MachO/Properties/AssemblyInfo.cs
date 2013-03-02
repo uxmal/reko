84beb2c20c93e462cf54374e7379cd67f59715c6
John Källén <uxmal@users.noreply.github.com>
2013-03-02 23:34:00 +0000

    * Releasing 0.2.4.

--- src/ImageLoaders/MachO/Properties/AssemblyInfo.cs
+++ src/ImageLoaders/MachO/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MachO")]
+[assembly: AssemblyTitle("MachO image file loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MachO")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct(Decompiler.AssemblyMetadata.Product)]
+[assembly: AssemblyCopyright(Decompiler.AssemblyMetadata.Copyright)]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion(Decompiler.AssemblyMetadata.AssemblyVersion)]
+[assembly: AssemblyFileVersion(Decompiler.AssemblyMetadata.AssemblyFileVersion)]
--- src/tools/xslt/AssemblyInfo.cs
+++ src/tools/xslt/AssemblyInfo.cs
@@ -46,8 +46,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("0.2.3.*")]
-[assembly: AssemblyFileVersion("0.2.3.*")]
+[assembly: AssemblyVersion("0.2.4.*")]
+[assembly: AssemblyFileVersion("0.2.4.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
