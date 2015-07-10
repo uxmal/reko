b0e9dad9626e7e85ba6acfb36ca955266c58e2ac
John Källén <uxmal@users.noreply.github.com>
2015-07-10 09:49:46 +0000

    HeuristicScanner passes all unit tests.
    New unit test fixture for HeuristicProcedureScanner

--- src/Decompiler/Scanning/AhoCorasickSearch.cs
+++ src/Decompiler/Scanning/AhoCorasickSearch.cs
@@ -659,4 +659,121 @@ namespace Decompiler.Scanning
             }
         }
     }
+
+    // Matching with *-wildcards via Aho-Corasick. Matching with ?-wildcards via convolutions.
+    //mport java.util.Arrays;
+
+    public static class ArrayEx
+    {
+        public static void Fill<T>(this T[] array, T value)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = value;
+            }
+        }
+    }
+    public class AhoCorasick
+    {
+
+        const int ALPHABET_SIZE = 26;
+
+        Node[] nodes;
+        int nodeCount;
+
+        private class Node
+        {
+            public int parent;
+            public char charFromParent;
+            public int suffLink = -1;
+            public int[] children = new int[ALPHABET_SIZE];
+            public int[] transitions = new int[ALPHABET_SIZE];
+            public bool leaf;
+
+            public Node()
+            {
+                children.Fill(-1);
+                transitions.Fill(-1);
+            }
+        }
+
+        public AhoCorasick(int maxNodes)
+        {
+            nodes = new Node[maxNodes];
+            // create root
+            nodes[0] = new Node();
+            nodes[0].suffLink = 0;
+            nodes[0].parent = -1;
+            nodeCount = 1;
+        }
+
+        public void addString(String s)
+        {
+            int cur = 0;
+            foreach (char ch in s)
+            {
+                int c = ch;
+                if (nodes[cur].children[c] == -1)
+                {
+                    nodes[nodeCount] = new Node();
+                    nodes[nodeCount].parent = cur;
+                    nodes[nodeCount].charFromParent = ch;
+                    nodes[cur].children[c] = nodeCount++;
+                }
+                cur = nodes[cur].children[c];
+            }
+            nodes[cur].leaf = true;
+        }
+
+        public int suffLink(int nodeIndex)
+        {
+            Node node = nodes[nodeIndex];
+            if (node.suffLink == -1)
+                node.suffLink = node.parent == 0 ? 0 : transition(suffLink(node.parent), node.charFromParent);
+            return node.suffLink;
+        }
+
+        public int transition(int nodeIndex, char ch)
+        {
+            int c = ch;
+            Node node = nodes[nodeIndex];
+            if (node.transitions[c] == -1)
+                node.transitions[c] = node.children[c] != -1 ? node.children[c] : (nodeIndex == 0 ? 0 : transition(suffLink(nodeIndex), ch));
+            return node.transitions[c];
+        }
+
+        // Usage example
+        public static void main(String[] args)
+        {
+            AhoCorasick ahoCorasick = new AhoCorasick(1000);
+            ahoCorasick.addString("bc");
+            ahoCorasick.addString("abc");
+
+            String s = "tabc";
+            int node = 0;
+            foreach (char ch in s)
+            {
+                node = ahoCorasick.transition(node, ch);
+            }
+            Console.Out.WriteLine(ahoCorasick.nodes[node].leaf);
+        }
+    }
+
+    public class Goo : IEnumerable
+    {
+        public void Add(string s)
+        {
+
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return null;
+        }
+
+        public static Goo Create()
+        {
+            return new Goo { "a", "B" };
+        }
+    }
 }
---
+++ src/UnitTests/Scanning/HeuristicTestBase.cs
@@ -0,0 +1,111 @@
+﻿#region License
+/* 
+ * Copyright (C) 1999-2015 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using Decompiler.Arch.X86;
+using Decompiler.Core;
+using Decompiler.Core.Types;
+using Rhino.Mocks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Decompiler.UnitTests.Scanning
+{
+    public class HeuristicTestBase
+    {
+        protected Program prog;
+        protected MockRepository mr;
+        protected IRewriterHost host;
+
+        public virtual void Setup()
+        {
+            mr = new MockRepository();
+        }
+
+        protected LoadedImage CreateImage(Address addr, params uint[] opcodes)
+        {
+            byte[] bytes = new byte[0x20];
+            var writer = new LeImageWriter(bytes);
+            uint offset = 0;
+            for (int i = 0; i < opcodes.Length; ++i, offset += 4)
+            {
+                writer.WriteLeUInt32(offset, opcodes[i]);
+            }
+            return new LoadedImage(addr, bytes);
+        }
+
+        protected void Given_RewriterHost()
+        {
+            host = mr.Stub<IRewriterHost>();
+            host.Stub(h => h.EnsurePseudoProcedure(null, null, 0))
+                .IgnoreArguments()
+                .Do(new Func<string, DataType, int, PseudoProcedure>((n, dt, a) =>
+                {
+                    return new PseudoProcedure(n, dt, a);
+                }));
+        }
+
+        protected void Given_Image32(uint addr, string sBytes)
+        {
+            var bytes = HexStringToBytes(sBytes);
+            var imag = new LoadedImage(Address.Ptr32(addr), bytes);
+            prog = new Program
+            {
+                Image = imag,
+                ImageMap = imag.CreateImageMap()
+            };
+        }
+
+        private static byte[] HexStringToBytes(string sBytes)
+        {
+            int chunkSize = 2;
+            var str = sBytes.Replace(" ", "");
+
+            return Enumerable.Range(0, str.Length / chunkSize)
+                .Select(i =>  (byte)Convert.ToInt16(
+                    str.Substring(i * chunkSize, chunkSize),
+                    16))
+                .ToArray();
+        }
+
+        protected void Given_x86_32()
+        {
+            prog.Architecture = new X86ArchitectureFlat32();
+        }
+
+        internal void Given_x86_16()
+        {
+            prog.Architecture = new X86ArchitectureReal();
+        }
+
+        internal void Given_ImageSeg(ushort seg, ushort offset, string sBytes)
+        {
+            var bytes = HexStringToBytes(sBytes);
+            var imag = new LoadedImage(Address.SegPtr(seg, offset), bytes);
+            prog = new Program
+            {
+                Image = imag,
+                ImageMap = imag.CreateImageMap()
+            };
+        }
+
+    }
+}
