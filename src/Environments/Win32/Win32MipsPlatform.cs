6273800c31d8c1d917e290abcde79158e88c6a20
uxmal <johnkal@gmail.com>
2015-09-08 09:16:30 +0200

    Created Win32MipsPlatform

---
+++ src/Environments/Win32/Win32MipsPlatform.cs
@@ -0,0 +1,69 @@
+﻿#region License
+/* 
+ * Copyright (C) 1999-2015 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using Reko.Core;
+using Reko.Core.Lib;
+using Reko.Core.Serialization;
+using Reko.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Reko.Environments.Win32
+{
+    public class Win32MipsPlatform : Win32Platform
+    {
+        public Win32MipsPlatform(IServiceProvider services, IProcessorArchitecture arch) : 
+            base(services, arch)
+        {
+        }
+
+        public override string DefaultCallingConvention
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public override Address AdjustProcedureAddress(Address addr)
+        {
+            return addr;
+        }
+
+        public override BitSet CreateImplicitArgumentRegisters()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ProcedureSerializer CreateProcedureSerializer(ISerializedTypeVisitor<DataType> typeLoader, string defaultConvention)
+        {
+            return new MipsProcedureSerializer(Architecture, typeLoader, defaultConvention);
+        }
+
+        public override ProcedureBase GetTrampolineDestination(ImageReader imageReader, IRewriterHost host)
+        {
+            return null;
+        }
+
+        public override ExternalProcedure LookupProcedureByName(string moduleName, string procName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
