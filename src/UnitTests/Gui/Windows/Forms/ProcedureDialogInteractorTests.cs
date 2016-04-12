948f75800d28b1ce7d996521aca560a60c4e267f
Pavel Tomin <_xyz_@mail.ru>
2016-04-12 21:34:02 +0500

    Remove "Parameters" tab from "Edit Procedure" dialog

--- src/UnitTests/Gui/Windows/Forms/ProcedureDialogInteractorTests.cs
+++ src/UnitTests/Gui/Windows/Forms/ProcedureDialogInteractorTests.cs
@@ -55,53 +55,6 @@ namespace Reko.UnitTests.Gui.Windows.Forms
         }
 
         [Test]
-        public void ReturnValue()
-        {
-            proc.Name = "x";
-            proc.Signature = new SerializedSignature();
-            proc.Signature.Arguments = new Argument_v1[0];
-            CreateReturnValue(proc.Signature);
-            //proc.Signature.Arguments = new SerializedArgument[1];
-            //proc.Signature.Arguments[0] = new SerializedArgument();
-            //proc.Signature.Arguments[0].Name = "arg0";
-            //proc.Signature.Arguments[0].Kind = new SerializedStackVariable(4);
-            //proc.Signature.Arguments[0].Type = "int32";
-
-            using (ProcedureDialog dlg = interactor.CreateDialog())
-            {
-                dlg.Show();
-                Assert.AreEqual(1, dlg.ArgumentList.Items.Count);
-                Assert.AreEqual("<Return value>", dlg.ArgumentList.Items[0].Text);
-            }
-        }
-
-        private static void CreateReturnValue(SerializedSignature sig)
-        {
-            sig.ReturnValue = new Argument_v1();
-            sig.ReturnValue.Kind = new Register_v1("eax");
-            sig.ReturnValue.Type = new TypeReference_v1("int32");
-        }
-
-        [Test]
-        public void SelectReturnValue()
-        {
-            proc.Name = "x";
-            proc.Signature = new SerializedSignature();
-            proc.Signature.Arguments = new Argument_v1[0];
-            CreateReturnValue(proc.Signature);
-            using (ProcedureDialog dlg = interactor.CreateDialog())
-            {
-                dlg.Show();
-                interactor.UserSwitchTab(1);
-                interactor.UserSelectedItem(dlg.ArgumentList, 0);
-
-                ListViewItem item = dlg.ArgumentList.Items[0];
-                Assert.AreEqual("<Return value>", item.Text);
-                Assert.AreSame(item.Tag, dlg.ArgumentProperties.SelectedObject);
-            }
-        }
-
-        [Test]
         public void PopulateWithEmptySignature()
         {
             proc.Name = "foo";
@@ -153,21 +106,10 @@ namespace Reko.UnitTests.Gui.Windows.Forms
             {
             }
 
-            public void UserSelectedItem(ListView listView, int p)
-            {
-                listView.Items[0].Selected = true;
-                base.ArgumentList_SelectedIndexChanged(listView, EventArgs.Empty);
-            }
-
             internal void UserChangeSignatureText( string text)
             {
                 dlg.Signature.Text = text;
                 base.Signature_TextChanged(dlg.Signature, EventArgs.Empty);
-            }
-
-            internal void UserSwitchTab(int p)
-            {
-                dlg.TabControl.SelectedIndex = p;
             }
         }
     }
