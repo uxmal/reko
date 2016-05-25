75742880b407c7efd66e3852724eaa34565c94a0
John Källén <johnkal@gmail.com>
2016-05-25 16:39:35 +0200

    Perform the CLEAN algorithm before structuring CFG

--- src/Decompiler/Structure/StructureAnalysis.cs
+++ src/Decompiler/Structure/StructureAnalysis.cs
@@ -64,6 +64,8 @@ namespace Reko.Structure
         {
             var ccc = new CompoundConditionCoalescer(proc);
             ccc.Transform();
+            var cfgc = new ControlFlowGraphCleaner(proc);
+            cfgc.Transform();
             var reg = Execute();
             //$REVIEW: yeecch. Should return the statements, and 
             // caller decides what to do with'em. Probably 
