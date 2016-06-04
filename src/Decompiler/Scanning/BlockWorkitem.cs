uxmal/reko

commit a4d7b7e421af311bf6222bd3ccac725f80348a68

Another bug found while addressing #237

src/Decompiler/Scanning/BlockWorkitem.cs
@@ -39,7 +39,8 @@ namespace Reko.Scanning
     /// Scanner work item for processing a basic block.
     /// </summary>
     /// <remarks>
-    /// The block work item will disassemble and rewrite instructions linearly until it reaches:
+    /// The block work item will disassemble and rewrite instructions linearly
+    /// until it reaches:
     /// <list type="">
     /// <item>An condition or unconditional branch</item>
     /// <item>A call to a procedure that is known to terminate.</item>
@@ -217,7 +218,7 @@ namespace Reko.Scanning
             // We don't know the 'then' block yet, as the following statements may chop up the block
             // we're presently in. Back-patch in when the block target is obtained.
             var branch = new Branch(b.Condition, new Block(blockCur.Procedure, "TMP!"));
-
+            
             // The following statements may chop up the blockCur, so hang on to the essentials.
             var proc = blockCur.Procedure;
             RtlInstructionCluster ricDelayed = null;
@@ -268,8 +269,8 @@ namespace Reko.Scanning
             else
             {
                 branch.Target = blockThen;      // The back-patch referred to above.
-                EnsureEdge(proc, branchingBlock, blockElse);
-                EnsureEdge(proc, branchingBlock, blockThen);
+                proc.ControlGraph.AddEdge(branchingBlock, blockElse);
+                proc.ControlGraph.AddEdge(branchingBlock, blockThen);
             }
 
             // Now, switch to the fallthru block and keep rewriting.
@@ -808,9 +809,10 @@ namespace Reko.Scanning
         {
             if (ri.NextStatementRequiresLabel)
             {
-                // Some machine instructions, like the X86 'rep cmps' instruction, force the need to generate 
-                // a label where there wouldn't be one normally, in the middle of the rtl sequence corresponding to
-                // the machine instruction.
+                // Some machine instructions, like the X86 'rep cmps' 
+                // instruction, force the need to generate a label where
+                // there wouldn't be one normally, in the middle of the rtl
+                // sequence corresponding to the machine instruction.
                 return AddIntraStatementBlock(proc);
             }
             else

src/Core/Expressions/ExpressionEmitter.cs
@@ -130,6 +130,12 @@ namespace Reko.Core.Expressions
             return new FieldAccess(dt, e, field);
         }
 
+        public FieldAccess Field(DataType dt, Expression e, string fieldName)
+        {
+            var field = new StructureField(0, dt, fieldName);
+            return new FieldAccess(dt, e, field);
+        }
+
         public Expression FMul(Expression a, Expression b)
         {
             return new BinaryExpression(Operator.FMul, a.DataType, a, b);

src/UnitTests/Scanning/BlockWorkitemTests.cs
@@ -741,5 +741,34 @@ testProc_exit:
 
             mr.VerifyAll();
         }
+
+        [Test(Description = "Create two edges even if they both point to the same destination")]
+        public void BwiBranchToSame()
+        {
+            var addrStart = Address.Ptr32(0x00100000);
+            var addrNext = Address.Ptr32(0x00100004);
+            var blockOther = new Block(proc, "other");
+
+            scanner.Stub(s => s.FindContainingBlock(addrStart)).Return(block);
+            scanner.Stub(s => s.FindContainingBlock(addrNext)).Return(blockOther);
+            scanner.Stub(s => s.GetTrace(null, null, null)).IgnoreArguments().Return(trace);
+            scanner.Stub(s => s.EnqueueJumpTarget(
+                Arg<Address>.Is.Equal(addrStart),
+                Arg<Address>.Is.Equal(addrNext),
+                Arg<Procedure>.Is.Same(proc),
+                Arg<ProcessorState>.Is.Anything)).Return(blockOther);
+            mr.ReplayAll();
+
+            trace.Add(m => m.Branch(m.LoadB(m.Word32(0x12340)), addrNext, RtlClass.ConditionalTransfer));
+
+            var wi = CreateWorkItem(addrStart, new FakeProcessorState(arch));
+            wi.Process();
+
+            Assert.AreEqual(2, block.Succ.Count);
+            Assert.AreSame(blockOther, block.Succ[0]);
+            Assert.AreSame(blockOther, block.Succ[1]);
+            mr.VerifyAll();
+
+        }
     }
 }

commit 9ccca28791f0d537e58339e3fd79026714ed30da

Fixed regression introduced in a4d7b7e421af311bf6222bd3ccac725f80348a68

src/Decompiler/Scanning/BlockWorkitem.cs
@@ -269,8 +269,11 @@ namespace Reko.Scanning
             else
             {
                 branch.Target = blockThen;      // The back-patch referred to above.
-                proc.ControlGraph.AddEdge(branchingBlock, blockElse);
-                proc.ControlGraph.AddEdge(branchingBlock, blockThen);
+                EnsureEdge(proc, branchingBlock, blockElse);
+                if (blockElse != blockThen)
+                    EnsureEdge(proc, branchingBlock, blockThen);
+                else
+                    proc.ControlGraph.AddEdge(branchingBlock, blockThen);
             }
 
             // Now, switch to the fallthru block and keep rewriting.
@@ -688,7 +691,7 @@ namespace Reko.Scanning
             return true;
         }
 
-        #endregion
+#endregion
 
         public bool ProcessIndirectControlTransfer(Address addrSwitch, RtlTransfer xfer)
         {

subjects/regression.log
@@ -177,14 +177,14 @@ l0800_9F9D_thunk_fn0800_9F9F: error: An error occurred while processing instruct
    at Reko.Analysis.ExpressionPropagator.GetStackDepthBeforeCall() in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\ExpressionPropagator.cs:line 130
    at Reko.Analysis.ExpressionPropagator.VisitCallInstruction(CallInstruction ci) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\ExpressionPropagator.cs:line 108
    at Reko.Core.Code.CallInstruction.Accept[T](InstructionVisitor`1 visitor) in D:\dev\uxmal\reko\master\src\Core\Code\CallInstruction.cs:line 66
-   at Reko.Analysis.TrashedRegisterFinder.RewriteBlock(Block block) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\TrashedRegisterFinder.cs:line 205
+   at Reko.Analysis.TrashedRegisterFinder.RewriteBlock(Block block) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\TrashedRegisterFinder.cs:line 201
 l0800_9F9D_thunk_fn0800_9F9F: error: An error occurred while processing instruction at address 0800:9F9D.
     The given key was not present in the dictionary.
        at System.Collections.Generic.Dictionary`2.get_Item(TKey key)
    at Reko.Analysis.ExpressionPropagator.GetStackDepthBeforeCall() in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\ExpressionPropagator.cs:line 130
    at Reko.Analysis.ExpressionPropagator.VisitCallInstruction(CallInstruction ci) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\ExpressionPropagator.cs:line 108
    at Reko.Core.Code.CallInstruction.Accept[T](InstructionVisitor`1 visitor) in D:\dev\uxmal\reko\master\src\Core\Code\CallInstruction.cs:line 66
-   at Reko.Analysis.TrashedRegisterFinder.RewriteBlock(Block block) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\TrashedRegisterFinder.cs:line 205
+   at Reko.Analysis.TrashedRegisterFinder.RewriteBlock(Block block) in D:\dev\uxmal\reko\master\src\Decompiler\Analysis\TrashedRegisterFinder.cs:line 201
 fn0800_889A: error: An error occurred during data flow analysis.
     The given key was not present in the dictionary.
        at System.Collections.Generic.Dictionary`2.get_Item(TKey key)
@@ -324,7 +324,7 @@ fn0800_889A: error: An error occurred while rewriting procedure to high-level la
        at Reko.Structure.RegionFactory.VisitDefInstruction(DefInstruction def) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 83
    at Reko.Core.Code.DefInstruction.Accept[T](InstructionVisitor`1 visitor) in D:\dev\uxmal\reko\master\src\Core\Code\Instruction.cs:line 89
    at Reko.Structure.RegionFactory.Create(Block b) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 46
-   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 189
+   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 191
    at Reko.Structure.StructureAnalysis.Execute() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 105
    at Reko.Structure.StructureAnalysis.Structure() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 69
    at Reko.DecompilerDriver.StructureProgram() in D:\dev\uxmal\reko\master\src\Decompiler\Decompiler.cs:line 533
@@ -333,7 +333,7 @@ fn0800_9F92: error: An error occurred while rewriting procedure to high-level la
        at Reko.Structure.RegionFactory.VisitDefInstruction(DefInstruction def) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 83
    at Reko.Core.Code.DefInstruction.Accept[T](InstructionVisitor`1 visitor) in D:\dev\uxmal\reko\master\src\Core\Code\Instruction.cs:line 89
    at Reko.Structure.RegionFactory.Create(Block b) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 46
-   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 189
+   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 191
    at Reko.Structure.StructureAnalysis.Execute() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 105
    at Reko.Structure.StructureAnalysis.Structure() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 69
    at Reko.DecompilerDriver.StructureProgram() in D:\dev\uxmal\reko\master\src\Decompiler\Decompiler.cs:line 533
@@ -342,7 +342,7 @@ fn0800_A96D: error: An error occurred while rewriting procedure to high-level la
        at Reko.Structure.RegionFactory.VisitDefInstruction(DefInstruction def) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 83
    at Reko.Core.Code.DefInstruction.Accept[T](InstructionVisitor`1 visitor) in D:\dev\uxmal\reko\master\src\Core\Code\Instruction.cs:line 89
    at Reko.Structure.RegionFactory.Create(Block b) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\RegionFactory.cs:line 46
-   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 189
+   at Reko.Structure.StructureAnalysis.BuildRegionGraph(Procedure proc) in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 191
    at Reko.Structure.StructureAnalysis.Execute() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 105
    at Reko.Structure.StructureAnalysis.Structure() in D:\dev\uxmal\reko\master\src\Decompiler\Structure\StructureAnalysis.cs:line 69
    at Reko.DecompilerDriver.StructureProgram() in D:\dev\uxmal\reko\master\src\Decompiler\Decompiler.cs:line 533