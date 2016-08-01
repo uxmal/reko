f9094c8499dab72e0f0c3d78de2bfd60005b3ec0
John Källén <johnkal@gmail.com>
2016-08-01 22:41:06 +0200

    Added fragment for unit testing #282

---
+++ src/tests/Fragments/regressions/r00282.asm
@@ -0,0 +1,21 @@
+﻿  ; reg00282
+  ; As reported by xor2003
+  ; The procedure should decompile as:
+  ; foo(byte al)
+  ; but reko decompiles it as:
+  ; foo(word ax, byte bl)
+  ; which is incorrect.
+
+  setmemallocstrat  proc       near  
+			push    ax
+			movzx   bx, al
+			mov     ax, 5801h
+			int     21h         ; DOS - 3+ - GET/SET MEMORY ALLOCATION STRATEGY
+				; AL = function code: set allocation strategy
+			pop     bx
+					shr     bx, 8
+			mov     ax, 5803h
+			int     21h         ; DOS - 3+ - GET/SET MEMORY ALLOCATION STRATEGY
+				; AL = function code: (DOS 5beta) set UMB link state
+			retn
+			endp
