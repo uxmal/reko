9af876d22e06b9fe2df122e2ee51676fcb20f6af
John Källén <johnkal@gmail.com>
2016-11-15 14:21:02 +0100

    Ran regression tests

--- src/Environments/SysV/SysVPlatform.cs
+++ src/Environments/SysV/SysVPlatform.cs
@@ -74,7 +74,6 @@ namespace Reko.Environments.SysV
                 return new Arm32ProcedureSerializer(Architecture, typeLoader, defaultConvention);
             case "m68k":
                 return new M68kProcedureSerializer(Architecture, typeLoader, defaultConvention);
-            
             default:
                 throw new NotImplementedException(string.Format("Procedure serializer for {0} not implemented yet.", Architecture.Description));
             }
