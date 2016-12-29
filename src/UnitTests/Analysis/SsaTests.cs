c07ca04ee8eecebfa4fb6ea16af358b13635d936
Pavel Tomin <_xyz_@mail.ru>
2016-12-29 09:44:29 +0500

    Add uses check to SsaTests

--- src/UnitTests/Analysis/SsaTests.cs
+++ src/UnitTests/Analysis/SsaTests.cs
@@ -175,6 +175,7 @@ namespace Reko.UnitTests.Analysis
                 ssa.Write(fut.TextWriter);
                 proc.Write(false, fut.TextWriter);
                 fut.AssertFilesEqual();
+                ssa.CheckUses(s => Assert.Fail(s));
             }
         }
 
@@ -216,6 +217,7 @@ namespace Reko.UnitTests.Analysis
 				ssa.Write(writer);
 				proc.Write(false, true, writer);
 				writer.WriteLine();
+                ssa.CheckUses(s => Assert.Fail(s));
 			}
 		}
 
