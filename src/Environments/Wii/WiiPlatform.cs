b729eadf3c685a7f8a795c7dfe7ec075dd3d666a
smx-smx <smxdev4@gmail.com>
2017-01-08 23:50:25 +0100

    Added preliminar Wii Environment and DOL ImageLoader

---
+++ src/Environments/Wii/WiiPlatform.cs
@@ -0,0 +1,58 @@
+﻿using Reko.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Reko.Core.CLanguage;
+using Reko.Core.Serialization;
+using Reko.Core.Types;
+using Reko.Arch.PowerPC;
+
+namespace Reko.Environments.Wii
+{
+	public class WiiPlatform : Platform {
+		public WiiPlatform(IServiceProvider services, IProcessorArchitecture arch, string platformId) : base(services, arch, "wii") {
+		}
+
+		public override string DefaultCallingConvention {
+			get {
+				return "";
+			}
+		}
+
+		public override HashSet<RegisterStorage> CreateImplicitArgumentRegisters() {
+			//$TODO: find out what registers are always preserved
+			return new HashSet<RegisterStorage>();
+		}
+
+		public override ProcedureSerializer CreateProcedureSerializer(ISerializedTypeVisitor<DataType> typeLoader, string defaultConvention) {
+			return new PowerPcProcedureSerializer(
+				(PowerPcArchitecture)Architecture,
+				typeLoader,
+				defaultConvention);
+		}
+
+		public override HashSet<RegisterStorage> CreateTrashedRegisters() {
+			//TODO: find out what registers are always trashed
+			return new HashSet<RegisterStorage>();
+		}
+
+		public override SystemService FindService(int vector, ProcessorState state) {
+			//$TODO: implement some services;
+			return null;
+		}
+
+		public override int GetByteSizeFromCBasicType(CBasicType cb) {
+			throw new NotImplementedException();
+		}
+
+		public override ProcedureBase GetTrampolineDestination(ImageReader imageReader, IRewriterHost host) {
+			return null;
+		}
+
+		public override ExternalProcedure LookupProcedureByName(string moduleName, string procName) {
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ src/ImageLoaders/Dol/DolLoader.cs
@@ -0,0 +1,148 @@
+﻿using Reko.Core;
+using Reko.Core.Configuration;
+using Reko.Environments.Wii;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.ImageLoaders.Dol
+{
+	/* Adapted from https://github.com/heinermann/ida-wii-loaders */
+	/* Format Reference: http://wiibrew.org/wiki/DOL */
+	public class DolLoader : ImageLoader {
+		private FileHeader hdr;
+		public DolLoader(IServiceProvider services, string filename, byte[] imgRaw) : base(services, filename, imgRaw) {
+		}
+
+		public override Address PreferredBaseAddress {
+			get {
+				return this.hdr.entrypoint;
+			}
+			set {
+				throw new NotImplementedException();
+			}
+		}
+
+		public override Program Load(Address addrLoad) {
+			BeImageReader rdr = new BeImageReader(this.RawImage, 0);
+			this.hdr = new FileHeader(rdr);
+
+			var cfgSvc = Services.RequireService<IConfigurationService>();
+			var arch = cfgSvc.GetArchitecture("ppc");
+
+			List<ImageSegment> segments = new List<ImageSegment>();
+			// add the code segment
+			for (uint i=0, snum=1; i<7; i++, snum++) {
+				if (hdr.addressText[i] == new Address32(0))
+					continue;
+				segments.Add(new ImageSegment(
+					snum.ToString(),
+					new MemoryArea(hdr.addressText[i], RawImage),
+					AccessMode.ReadWriteExecute
+				));
+			}
+
+			// create all data segments
+			for (uint i = 0, snum = 1; i < 11; i++, snum++) {
+				if (hdr.addressData[i] == new Address32(0))
+					continue;
+				segments.Add(new ImageSegment(
+					snum.ToString(),
+					new MemoryArea(hdr.addressText[i], RawImage),
+					AccessMode.ReadWriteExecute
+				));
+			}
+
+			if (hdr.addressBSS != new Address32(0)) {
+				segments.Add(new ImageSegment(
+					".bss",
+					new MemoryArea(hdr.addressBSS, RawImage),
+					AccessMode.ReadWrite
+				));
+			}
+
+
+			SegmentMap segmentMap = new SegmentMap(hdr.entrypoint, segments.ToArray());
+
+			return new Program(
+				segmentMap,
+				arch,
+				new WiiPlatform(Services, arch, "Wii")
+			);
+		}
+
+		public override RelocationResults Relocate(Program program, Address addrLoad) {
+			throw new NotImplementedException();
+		}
+
+		public class FileHeader {
+			public uint[] offsetText = new uint[7];
+			public uint[] offsetData = new uint[11];
+			public Address32[] addressText = new Address32[7];
+			public Address32[] addressData = new Address32[11];
+			public uint[] sizeText = new uint[7];
+			public uint[] sizeData = new uint[11];
+			public Address32 addressBSS;
+			public uint sizeBSS;
+			public Address32 entrypoint;
+
+
+			public FileHeader(BeImageReader rdr) {
+				uint uAddress;
+
+				bool result;
+				for (uint i = 0; i < 7; i++) {
+					result = rdr.TryReadUInt32(out offsetText[i]);
+					if (!result)
+						goto fail;
+				}
+				for (uint i = 0; i < 11; i++) {
+					result = rdr.TryReadUInt32(out offsetData[i]);
+					if (!result)
+						goto fail;
+				}
+				for (uint i = 0; i < 7; i++) {
+					result = rdr.TryReadUInt32(out uAddress);
+					if (!result)
+						goto fail;
+					addressText[i] = new Address32(uAddress);
+				}
+				for (uint i = 0; i < 11; i++) {
+					result = rdr.TryReadUInt32(out uAddress);
+					if (!result)
+						goto fail;
+					addressData[i] = new Address32(uAddress);
+				}
+				for (uint i = 0; i < 7; i++) {
+					result = rdr.TryReadUInt32(out sizeText[i]);
+					if (!result)
+						goto fail;
+				}
+				for (uint i = 0; i < 11; i++) {
+					result = rdr.TryReadUInt32(out sizeData[i]);
+					if (!result)
+						goto fail;
+				}
+
+				result = rdr.TryReadUInt32(out uAddress);
+				if (!result)
+					goto fail;
+				this.addressBSS = new Address32(uAddress);
+
+				result = rdr.TryReadUInt32(out sizeBSS);
+				if (!result)
+					goto fail;
+
+				result = rdr.TryReadUInt32(out uAddress);
+				if (!result)
+					goto fail;
+				this.entrypoint = new Address32(uAddress);
+
+				fail:
+				throw new BadImageFormatException("Invalid DOL header.");
+			}
+		}
+	}
+}
