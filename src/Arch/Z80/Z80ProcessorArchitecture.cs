b777cfdaca96972f3a542c77667b745f8323d747
John Källén <johnkal@gmail.com>
2017-02-01 08:04:33 +0100

    Added Z80InstructionComparer.

--- src/Arch/Z80/Z80ProcessorArchitecture.cs
+++ src/Arch/Z80/Z80ProcessorArchitecture.cs
@@ -79,7 +79,7 @@ namespace Reko.Arch.Z80
 
         public override IEqualityComparer<MachineInstruction> CreateInstructionComparer(Normalize norm)
         {
-            throw new NotImplementedException();
+            return new Z80InstructionComparer(norm);
         }
 
         public override ProcessorState CreateProcessorState()
@@ -258,6 +258,7 @@ namespace Reko.Arch.Z80
         public static readonly RegisterStorage bc_ = new RegisterStorage("bc'", 12, 0, PrimitiveType.Word16);
         public static readonly RegisterStorage de_ = new RegisterStorage("de'", 13, 0, PrimitiveType.Word16);
         public static readonly RegisterStorage hl_ = new RegisterStorage("hl'", 14, 0, PrimitiveType.Word16);
+        public static readonly RegisterStorage af_ = new RegisterStorage("af'", 15, 0, PrimitiveType.Word16);
 
         public static readonly RegisterStorage S = new RegisterStorage("S", 24, 0, PrimitiveType.Bool);
         public static readonly RegisterStorage Z = new RegisterStorage("Z", 25, 0, PrimitiveType.Bool);
