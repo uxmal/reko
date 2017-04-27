616787de949f0cf8e2cb8e908de5139890566695
John Källén <johnkal@gmail.com>
2017-04-27 14:02:04 +0200

    IR parser

---
+++ src/Core/IRFormat/IRProcedureBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Reko.Core.Code;
+using Reko.Core.Expressions;
+
+namespace Reko.Core.IRFormat
+{
+    internal class IRProcedureBuilder : CodeEmitter
+    {
+        private object arch;
+        private string name;
+
+        public IRProcedureBuilder(string name, object arch)
+        {
+            this.name = name;
+            this.arch = arch;
+        }
+
+        public override Frame Frame
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override Statement Emit(Instruction instr)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Identifier Register(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
