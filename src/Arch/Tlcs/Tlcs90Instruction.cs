c8e174b27be806ed4ea25f8907b47ea448922c95
John Källén <johnkal@gmail.com>
2017-05-05 16:14:09 +0200

    Reorganized TLCS project

---
+++ src/Arch/Tlcs/Tlcs90Instruction.cs
@@ -0,0 +1,38 @@
+﻿using Reko.Core.Machine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.Arch.Tlcs
+{
+    public class Tlcs90Instruction : MachineInstruction
+    {
+        public override InstructionClass InstructionClass 
+        {
+            get { return InstructionClass.Linear; }
+        }
+
+        public override bool IsValid
+        {
+            get { }
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override int OpcodeAsInteger
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override MachineOperand GetOperand(int i)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
