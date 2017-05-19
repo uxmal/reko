3fbf12b0e80667d741f079a673519719bfe42eb7
John Källén <johnkal@gmail.com>
2017-05-19 16:38:24 +0200

    INativeTypeFactory

---
+++ src/Core/NativeInterface/INativeTypeFactory.cs
@@ -0,0 +1,44 @@
+﻿#region License
+/* 
+ * Copyright (C) 1999-2017 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.Core.NativeInterface
+{
+    public interface INativeTypeFactory
+    {
+        [PreserveSig] HExpr ArrayOf(HExpr dt, int length);
+
+        [PreserveSig] HExpr PtrTo(HExpr dt, int byte_size);
+
+        [PreserveSig] void BeginStruct(HExpr dt, int byte_size);
+        [PreserveSig] void Field(HExpr dt, int offset, string name);
+        [PreserveSig] HExpr EndStruct();
+
+        [PreserveSig] void BeginFunc(HExpr dt, int byte_size);
+        [PreserveSig] void Parameter(HExpr dt, string name);
+        [PreserveSig] HExpr EndFunc();
+    }
+}
