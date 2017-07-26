1b1ca425d2a72b7c3d28b3a4f932061206093ddc
John Källén <johnkal@gmail.com>
2017-07-26 13:17:55 +0200

    Ran regression tests

--- src/Decompiler/Typing/TypeAnalyzer.cs
+++ src/Decompiler/Typing/TypeAnalyzer.cs
@@ -81,7 +81,7 @@ namespace Reko.Typing
             ctn = new ComplexTypeNamer();
             ter = new TypedExpressionRewriter(program, eventListener);
 
-            RestrictProcedures(program, 0, 60, true); // Re-enable this for debugging
+            // RestrictProcedures(program, 0, 60, true); // Re-enable this for debugging
             eventListener.ShowStatus("Gathering primitive datatypes from instructions.");
 			aen.Transform(program);
             eqb.Build(program);
