d1bd73b566b94ccd181b800984d65d5220318c5f
Stefano Moioli <smxdev4@gmail.com>
2017-10-01 01:35:44 +0200

    WIP

--- src/UserInterfaces/Electron/app/app.ts
+++ src/UserInterfaces/Electron/app/app.ts
@@ -1,10 +1,28 @@
 import {ipcRenderer} from 'electron';
 import $ = require("jquery");
 
+import Program from './lib/Program';
+import Browser from './lib/Browser';
+
+var browser:Browser = new Browser();
 
 function setup(){
 	ipcRenderer.on("procedure", (event:any, arg:any) => {
 		$(".reko-procedures").html(arg);
+		browser.update();
+	});
+
+	ipcRenderer.on("project", (event:any, arg:any) => {
+		var proj = JSON.parse(arg);
+
+		var node = $(".reko-browser");
+
+		proj.programs.forEach((prog:any) => {
+			node.append(new Program(prog.name, prog.procedures).render());
+		});
+
+
+		browser.update();
 	});
 
 	$("#btn-test").click(function(e){
