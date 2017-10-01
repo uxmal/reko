4bfa723adfaab4b87b8b8d5361d8950889bfbe97
Stefano Moioli <smxdev4@gmail.com>
2017-10-01 20:46:38 +0200

    getAssemblies: copy all dlls, xml and config files

--- src/UserInterfaces/Electron/getAssemblies.bat
+++ src/UserInterfaces/Electron/getAssemblies.bat
@@ -3,4 +3,6 @@ rem Copies .NET assemblies and other shared libraries from the Reko solution int
 rem the `generated\assemblies` folder so they are visible to the Electron application.
 
 xcopy /d /y ..\Electron.Adapter\bin\debug\Reko.Gui.Electron.Adapter.dll generated\assemblies
-xcopy /d /y ..\..\Core\bin\debug\Reko.Core.dll generated\assemblies
+xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.dll generated\assemblies
+xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.xml generated\assemblies
+xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.config generated\assemblies
