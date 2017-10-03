b4819af5648f46f0b77d959829fe4f01b6d8ce5c
Stefano Moioli <smxdev4@gmail.com>
2017-10-04 01:11:18 +0200

    TemplateLoader: added caching

--- src/UserInterfaces/Electron/app/TemplateLoader.ts
+++ src/UserInterfaces/Electron/app/TemplateLoader.ts
@@ -3,9 +3,18 @@ import * as fs from 'fs';
 import * as path from 'path';	
 
 export default class TemplateLoader {
+	private static Templates:Map<string, HandlebarsTemplateDelegate<any>> = new Map<string, HandlebarsTemplateDelegate<any>>();
+
 	public static LoadTemplate(name:string){
+		var cached = TemplateLoader.Templates.get(name);
+		if(typeof(cached) !== 'undefined')
+			return cached;
+
 		var tplPath = path.resolve(__dirname + `/../views/${name}.tpl`);
 		var data = fs.readFileSync(tplPath).toString();
-		return handlebars.compile(data);
+		var tpl = handlebars.compile(data)
+		TemplateLoader.Templates.set(name, tpl);
+		
+		return tpl;
 	}
 }
