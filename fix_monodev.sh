4018309c70c541ae6286b4029269942f36a3988d
Stefano Moioli <smxdev4@gmail.com>
2017-11-20 00:32:42 +0100

    fix_monodev.sh: cd to script directory at start

--- fix_monodev.sh
+++ fix_monodev.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 find . -type f -iname "*.csproj" -print0 | while IFS= read -r -d '' proj; do
 	sed 's/%24/\$/g;s/%29/\)/g;s/%28/\(/g' -i "$proj"
 	echo "Done $proj"
