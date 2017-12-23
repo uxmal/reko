37e33eda93aa9af6471bc5718f9b42aa34198964
Pavel Tomin <_xyz_@mail.ru>
2017-12-23 20:00:16 +0500

    Create structure analysis unit test to reproduce error in the last resort refinement of switch

--- src/UnitTests/Structure/StructureAnalysisTests.cs
+++ src/UnitTests/Structure/StructureAnalysisTests.cs
@@ -735,6 +735,69 @@ case_0:
             RunTest(sExp, m.Procedure);
         }
 
+        [Test]
+        public void StrAnls_Switch_LastResort()
+        {
+            var r1 = m.Reg32("r1", 1);
+
+            m.Switch(r1, "case_0", "case_1", "case_2");
+
+            m.Label("case_0");
+            m.Assign(r1, 3);
+            m.Goto("done");
+
+            m.Label("case_1");
+            m.Assign(r1, 2);
+            m.BranchIf(m.Fn("needFullCheck"), "fullCheck");
+
+            m.BranchIf(m.Fn("fastCheckFailed"), "failed");
+            m.Goto("done");
+
+            m.Label("fullCheck");
+            m.BranchIf(m.Fn("fullCheckFailed"), "failed");
+            m.Goto("done");
+
+            m.Label("failed");
+            m.SideEffect(m.Fn("throwError"));
+            m.Goto("done");
+
+            m.Label("case_2");
+            m.Assign(r1, 1);
+            m.Goto("done");
+
+            m.Label("done");
+            m.SideEffect(m.Fn("finalize"));
+            m.Return(r1);
+
+            var sExp =
+@"    switch (r1)
+    {
+    case 0x00:
+        r1 = 0x03;
+        break;
+    case 0x01:
+        r1 = 0x02;
+        if (!needFullCheck())
+        {
+            if (fastCheckFailed())
+                goto failed;
+        }
+        else if (fullCheckFailed())
+        {
+failed:
+            throwError();
+        }
+        break;
+    case 0x02:
+        r1 = 0x01;
+        break;
+    }
+    finalize();
+    return r1;
+";
+            RunTest(sExp, m.Procedure);
+        }
+
         [Test(Description="A do-while with a nested if-then-else")]
         public void StrAnls_DoWhile_NestedIfElse()
         {
