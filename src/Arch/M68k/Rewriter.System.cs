0fde3e18ff01b6f84ea11b14342b0d4c5db023ca
John Källén <johnkal@gmail.com>
2018-02-09 12:02:52 +0100

    Repairs to massive merge
    UnitTests are behaving flakily in the VS2017 Test Explorer.

---
+++ src/Arch/M68k/Rewriter.System.cs
@@ -0,0 +1,79 @@
+﻿#region License
+/* 
+ * Copyright (C) 1999-2018 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using Reko.Core.Rtl;
+using Reko.Core.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reko.Arch.M68k
+{
+    public partial class Rewriter
+    {
+        private void RewriteBkpt()
+        {
+            rtlc = RtlClass.Invalid;
+            var src = this.orw.RewriteSrc(di.op1, di.Address);
+            m.SideEffect(host.PseudoProcedure("__bkpt", VoidType.Instance, src));
+        }
+
+        private void RewriteMoves()
+        {
+            rtlc = RtlClass.System;
+            var src = this.orw.RewriteSrc(di.op1, di.Address);
+            var dst = orw.RewriteDst(di.op2, di.Address, di.dataWidth, src, (s, d) =>
+                host.PseudoProcedure("__moves", VoidType.Instance, s));
+        }
+
+        private void RewritePflushr()
+        {
+            rtlc = RtlClass.System;
+            var src = this.orw.RewriteSrc(di.op1, di.Address);
+            m.SideEffect(host.PseudoProcedure("__pflushr", VoidType.Instance, src));
+        }
+
+        private void RewritePtest()
+        {
+            rtlc = RtlClass.System;
+            var src1 = this.orw.RewriteSrc(di.op1, di.Address);
+            var src2 = this.orw.RewriteSrc(di.op2, di.Address);
+            m.SideEffect(host.PseudoProcedure("__ptest", VoidType.Instance, src2, src1));
+        }
+
+        private void RewriteRte()
+        {
+            rtlc = RtlClass.System;
+            var sp = binder.EnsureRegister(Registers.a7);
+            var sr = binder.EnsureRegister(Registers.sr);
+            m.Assign(sr, m.Mem16(sp));
+            m.Assign(sp, m.IAdd(sp, m.Int32(2)));
+            m.Return(4, 0);
+        }
+
+        private void RewriteTas()
+        {
+            //$TODO: need to implement this using operand rewriter.
+            EmitInvalid();
+        }
+    }
+}
---
+++ src/UnitTests/Structure/AbsynCodeEmitter.cs
@@ -0,0 +1,78 @@
+﻿#region License
+/* 
+ * Copyright (C) 1999-2018 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using Reko.Core.Absyn;
+using Reko.Core.Expressions;
+using Reko.Core;
+using Reko.Core.Code;
+
+namespace Reko.UnitTests.Structure
+{
+    internal class AbsynCodeEmitter : ExpressionEmitter
+    {
+        private List<AbsynStatement> stmts;
+
+        public AbsynCodeEmitter(List<AbsynStatement> stmts)
+        {
+            this.stmts = stmts;
+        }
+
+        public void Assign(Expression dst, Expression src)
+        {
+            var ass = new AbsynAssignment(dst, src);
+            stmts.Add(ass);
+        }
+
+        public void Return()
+        {
+            var ret = new AbsynReturn(null);
+            stmts.Add(ret);
+        }
+
+        public void If(Expression id, Action<AbsynCodeEmitter> then)
+        {
+            var thenStmts = new List<AbsynStatement>();
+            var thenEmitter = new AbsynCodeEmitter(thenStmts);
+            then(thenEmitter);
+            stmts.Add(new AbsynIf(id, thenStmts));
+        }
+
+        public void If(Expression id, Action<AbsynCodeEmitter> then, Action<AbsynCodeEmitter> els)
+        {
+            var thenStmts = new List<AbsynStatement>();
+            var thenEmitter = new AbsynCodeEmitter(thenStmts);
+            then(thenEmitter);
+            var elseStmts = new List<AbsynStatement>();
+            var elseEmitter = new AbsynCodeEmitter(elseStmts);
+            els(elseEmitter);
+            stmts.Add(new AbsynIf(id, thenStmts, elseStmts));
+        }
+
+        public void SideEffect(Expression e)
+        {
+            var s = new AbsynSideEffect(e);
+            stmts.Add(s);
+        }
+
+      
+    }
+}
