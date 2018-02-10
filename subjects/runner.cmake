c8601b63e3ece2fb50c54c082ef6675400cd9c7d
Stefano Moioli <smxdev4@gmail.com>
2018-02-10 21:37:33 +0100

    CMake_regressions: removed dcproject parsing

--- subjects/runner.cmake
+++ subjects/runner.cmake
@@ -20,23 +20,3 @@ execute_process(
 	COMMAND ${COMMAND}
 	WORKING_DIRECTORY ${TEST_DIRECTORY}
 )
-
-if(OUT_DASM AND NOT EXISTS "${OUT_DASM}")
-	message(FATAL_ERROR "${OUT_DASM} hasn't been generated")
-endif()
-
-if(OUT_DIS AND NOT EXISTS "${OUT_DIS}")
-	message(FATAL_ERROR "${OUT_DIS} hasn't been generated")
-endif()
-
-if(OUT_SRC AND NOT EXISTS "${OUT_SRC}")
-	message(FATAL_ERROR "${OUT_SRC} hasn't been generated")
-endif()
-
-if(OUT_HDR AND NOT EXISTS "${OUT_HDR}")
-	message(FATAL_ERROR "${OUT_HDR} hasn't been generated")
-endif()
-
-if(OUT_GLOBALS AND NOT EXISTS "${OUT_GLOBALS}")
-	message(FATAL_ERROR "${OUT_GLOBALS} hasn't been generated")
-endif()
