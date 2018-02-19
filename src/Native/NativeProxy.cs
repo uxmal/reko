e97276f27e77ae307336ca0c0cb58b89cf9ad380
John Källén <johnkal@gmail.com>
2018-02-20 00:49:42 +0100

    NativeProxy.csproj
    Proxy project to kick off the Native CMake sub-build.

---
+++ src/Native/NativeProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NativeProxy
+{
+    public class NativeProxy
+    {
+        // This class is here only for the compiler to have something to do.
+    }
+}
