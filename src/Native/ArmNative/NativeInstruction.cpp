69c9479aa4dae0e21da6f26fae0a846955e52b75
John Källén <johnkal@gmail.com>
2018-02-19 18:03:10 +0100

    Second attempt: replace strcat_s with strcat

--- src/Native/ArmNative/NativeInstruction.cpp
+++ src/Native/ArmNative/NativeInstruction.cpp
@@ -253,7 +253,7 @@ void NativeInstruction::Write(const cs_insn & insn, const cs_arm_op & op, INativ
 		snprintf(risky, sizeof(risky), "#%lf", op.fp);
 		if (strcspn(risky, nosuffixRequired) == strlen(risky))
 		{
-			strcat_s(risky, sizeof(risky), ".0");
+			strcat(risky, ".0");
 		}
 		writer.WriteString(risky);
 		break;
