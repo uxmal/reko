0d774e4c1449a8eea8f2340df709dccffa4a4197
Stefano Moioli <smxdev4@gmail.com>
2018-02-21 14:09:49 +0100

    CMake: Reworked REKO_PLATFORM, support 32bit multiarch

--- src/Native/reko.cmake
+++ src/Native/reko.cmake
@@ -71,6 +71,10 @@ function(invoke_cmake name path build_dir)
 		list(APPEND CMAKE_ARGS "-DREKO_PATH=${REKO_PATH}")
 	endif()
 
+	if(REKO_PLATFORM)
+		list(APPEND CMAKE_ARGS "-DREKO_PLATFORM=${REKO_PLATFORM}")
+	endif()
+
 	if(CMAKE_BUILD_TYPE)
 		list(APPEND CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
 	endif()
