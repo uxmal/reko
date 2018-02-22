f1f4bf4d7b6b93362606d443ad847f3c8b549d76
chostelet <christian.hostelet@free.fr>
2018-02-22 22:24:38 +0100

    Introducing Intel HEX32 image loader

---
+++ src/ImageLoaders/IHex32/IHex32Parser.cs
@@ -0,0 +1,107 @@
+﻿#region License
+/* 
+ * Copyright (C) 2017-2018 Christian Hostelet.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Reko.ImageLoaders.IHex32
+{
+    public class IHEX32Parser
+    {
+        /// <summary>
+        /// Parse a single IHex32 hexadecimal record.
+        /// </summary>
+        /// <param name="hexRecord">The hexadecimal record as a string.</param>
+        /// <param name="lineNum">(Optional) The line number in the IHex32 binary stream.</param>
+        /// <returns>
+        /// An <see cref="IHex32Record"/> .
+        /// </returns>
+        /// <exception cref="IHEX32Exception">Thrown whenever an error is found in the IHex32 record.</exception>
+        public static IHex32Record ParseRecord(string hexRecord, int lineNum = 0)
+        {
+            if (hexRecord == null)
+                throw new IHEX32Exception("Hex record line can not be null", lineNum);
+            if (hexRecord.Length < 11)
+                throw new IHEX32Exception($"Hex record line length [{hexRecord}] is less than 11", lineNum);
+            if (hexRecord.Length % 2 == 0)
+                throw new IHEX32Exception($"Hex record has an even number of characters [{hexRecord}]", lineNum);
+            if (!hexRecord.StartsWith(":"))
+                throw new IHEX32Exception($"Illegal line start character [{hexRecord}]", lineNum);
+            var hexData = _tryParseData(hexRecord.Substring(1), lineNum);
+
+            if (hexData.Count != hexData[0] + 5)
+                throw new IHEX32Exception($"Line [{hexRecord}] does not have required record length of [{hexData[0] + 5}]", lineNum);
+
+            if (!Enum.IsDefined(typeof(IHEX32RecordType), hexData[3]))
+                throw new IHEX32Exception($"Invalid record type value: [{hexData[3]}]", lineNum);
+
+            var checkSum = hexData.Last();
+            hexData.RemoveAt(hexData[0] + 4);
+
+            if (!_verifyChecksum(hexData, checkSum))
+                throw new IHEX32Exception($"Checksum for line [{hexRecord}] is incorrect", lineNum);
+
+            var dataSize = hexData[0];
+
+            var newRecord = new IHex32Record
+            {
+                ByteCount = dataSize,
+                Address = ((uint)(hexData[1] << 8) | hexData[2]),
+                RecordType = (IHEX32RecordType)hexData[3],
+                Data = hexData,
+                CheckSum = checkSum
+            };
+
+            hexData.RemoveRange(0, 4);
+
+            return newRecord;
+        }
+
+        #region Helpers
+
+        private static bool _verifyChecksum(IList<byte> checkSumData, int checkSum)
+        {
+            var maskedSumBytes = checkSumData.Sum(x => x) & 0xff;
+            var calculatedChecksum = (byte)(256 - maskedSumBytes);
+
+            return calculatedChecksum == checkSum;
+        }
+
+        private static List<byte> _tryParseData(string hexData, int lineNum = 0)
+        {
+            try
+            {
+                return Enumerable.Range(0, hexData.Length)
+                    .Where(i => (i & 1) == 0)
+                    .Select(i => Convert.ToByte(hexData.Substring(i, 2), 16))
+                    .ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new IHEX32Exception($"Unable to parse bytes for [{hexData}]", ex, lineNum);
+            }
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ src/ImageLoaders/IHex32/IHex32Reader.cs
@@ -0,0 +1,173 @@
+﻿#region License
+/* 
+ * Copyright (C) 2017-2018 Christian Hostelet.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion
+
+using System;
+using System.IO;
+
+namespace Reko.ImageLoaders.IHex32
+{
+
+    /// <summary>
+    /// A reader capable of reading a Intel HEX32 stream
+    /// </summary>
+    public class IHEX32Reader : IDisposable
+    {
+
+        #region Locals
+
+        private readonly StreamReader _streamReader;
+        private int _linenum;
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Constructs instance of an <see cref="IHEX32Reader" />.
+        /// </summary>
+        /// <param name="str">The source stream of the hex file.</param>
+        /// <exception cref="ArgumentNullException">If the <paramref name="str" /> is null.</exception>
+        public IHEX32Reader(Stream str)
+        {
+            if (str == null)
+                throw new ArgumentNullException(nameof(str));
+            _streamReader = new StreamReader(str);
+            _linenum = 0;
+        }
+
+        #endregion
+
+        #region IDisposable implementation
+
+        private bool _disposedValue; // To detect redundant calls
+
+        /// <summary>
+        /// Dispose the <see cref="IHEX32Reader"/>
+        /// </summary>
+        /// <param name="disposing"></param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!_disposedValue)
+            {
+                if (disposing)
+                {
+                    _streamReader?.Dispose();
+                }
+
+                _disposedValue = true;
+            }
+        }
+
+        /// <summary>
+        /// Dispose the <see cref="IHEX32Reader"/>
+        /// </summary>
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(disposing) above.
+            Dispose(true);
+        }
+
+        #endregion
+
+        #region Public Methods/properties
+
+        /// <summary>
+        /// Gets or sets the base address for loading the 16-bit IHex32 records.
+        /// </summary>
+        public uint AddressBase { get; set; } = 0;
+
+        /// <summary>
+        /// Read data with address information from the stream.
+        /// </summary>
+        /// <param name="address">[out] The start address for the block of data.</param>
+        /// <param name="data">[out] The data byte block.</param>
+        /// <returns>
+        /// true if there are more records available or false if end of file.
+        /// </returns>
+        /// <remarks>
+        /// An address value may be read without corresponding data bytes. This occurs for record
+        /// types 02, 04, 05.
+        /// </remarks>
+        public bool Read(out uint address, out byte[] data)
+        {
+            data = null;
+            address = 0;
+
+            string hexLine = null;
+            while (!_streamReader.EndOfStream && string.IsNullOrWhiteSpace(hexLine))
+            {
+                _linenum++;
+                hexLine = _streamReader.ReadLine();
+            }
+            if (_streamReader.EndOfStream) return false;
+
+            var hexRecord = IHEX32Parser.ParseRecord(hexLine, _linenum);
+
+            if (hexRecord.RecordType != IHEX32RecordType.EndOfFile)
+            {
+                address = _handleAddress(hexRecord);
+
+                if (hexRecord.RecordType == IHEX32RecordType.Data)
+                    data = hexRecord.Data.ToArray();
+
+                return true;
+            }
+
+            return false;
+        }
+
+        #endregion
+
+        #region Helpers
+
+        private uint _handleAddress(IHex32Record hexRecord)
+        {
+            switch (hexRecord.RecordType)
+            {
+                case IHEX32RecordType.Data:
+                    return AddressBase + hexRecord.Address;
+
+                case IHEX32RecordType.ExtendedSegmentAddress:
+                    AddressBase = (((uint)hexRecord.Data[0] << 8) | hexRecord.Data[1]) << 4;
+                    return AddressBase;
+
+                case IHEX32RecordType.StartSegmentAddress:
+                    return AddressBase;
+
+                case IHEX32RecordType.ExtendedLinearAddress:
+                    AddressBase = (((uint)hexRecord.Data[0] << 8) | hexRecord.Data[1]) << 16;
+                    return AddressBase;
+
+                case IHEX32RecordType.StartLinearAddress:
+                    AddressBase = (((uint)hexRecord.Data[0] << 24) | ((uint)hexRecord.Data[1] << 16) |
+                                           ((uint)hexRecord.Data[2] << 8) | hexRecord.Data[3]);
+                    return AddressBase;
+
+                default:
+                    throw new IHEX32Exception($"Unknown value read for [{nameof(hexRecord.RecordType)}]", _linenum);
+            }
+
+        }
+
+        #endregion
+
+    }
+
+}
