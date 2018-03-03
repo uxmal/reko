0f68c9dc342bdd98a3c69c18474174f8a4a41f23
Pavel Tomin <_xyz_@mail.ru>
2018-03-03 21:21:47 +0500

    Create scanner test with common jump

--- src/UnitTests/Scanning/ScannerTests.cs
+++ src/UnitTests/Scanning/ScannerTests.cs
@@ -114,12 +114,13 @@ namespace Reko.UnitTests.Scanning
             scan.EnqueueImageSymbol(sym, true);
         }
 
-        private void AssertProgram(string sExpected, Program prog)
+        private void AssertProgram(
+            string sExpected, bool showEdges, Program prog)
         {
             var sw = new StringWriter();
             foreach (var proc in prog.Procedures.Values)
             {
-                proc.Write(false, false, sw);
+                proc.Write(false, showEdges, sw);
                 sw.WriteLine();
             }
             var sActual = sw.ToString();
@@ -128,6 +129,11 @@ namespace Reko.UnitTests.Scanning
                 Debug.WriteLine(sActual);
                 Assert.AreEqual(sExpected, sActual);
             }
+        }
+
+        private void AssertProgram(string sExpected, Program prog)
+        {
+            AssertProgram(sExpected, false, prog);
         }
 
         private SerializedType Char()
@@ -512,6 +518,56 @@ fn00001100_exit:
 
 ";
             AssertProgram(sExp, program);
+        }
+
+        [Test]
+        public void Scanner_CommonJump()
+        {
+            var scan = CreateScanner(0x1000, 0x2000);
+            Given_Trace(new RtlTrace(0x1000)
+            {
+                // 0x1000:
+                m => { m.Assign(reg1, m.Word32(0)); },
+                // 0x1004:
+                m => { m.Goto(Address.Ptr32(0x1100)); }
+            });
+            Given_Trace(new RtlTrace(0x1100)
+            {
+                // 0x1100:
+                m => { m.Assign(reg1, m.Word32(1)); },
+                // 0x1104:
+                m => { m.Goto(Address.Ptr32(0x1004)); },
+            });
+            fakeArch.Test_IgnoreAllUnkownTraces();
+
+            scan.EnqueueImageSymbol(
+                new ImageSymbol(Address.Ptr32(0x1000))
+                {
+                    ProcessorState = arch.CreateProcessorState(),
+                },
+                true);
+            scan.ScanImage();
+
+            var sExp =
+@"// fn00001000
+// Return size: 0
+void fn00001000()
+fn00001000_entry:
+	// succ:  l00001000
+l00001000:
+	r63 = fp
+	r1 = 0x00000000
+	// succ:  l00001004
+l00001004:
+	// succ:  l00001100
+l00001100:
+	r1 = 0x00000001
+	goto l00001004
+	// succ:  l00001004
+fn00001000_exit:
+
+";
+            AssertProgram(sExp, true, program);
         }
 
         [Test]
