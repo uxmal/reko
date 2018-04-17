37cde28a5eed0d8c9e504fac40b6e5beec05c7b6
Stefano Moioli <smxdev4@gmail.com>
2018-04-17 21:23:33 +0200

    Pe2Lib: Rewritten parser. Add PDB demangled names as comments

--- src/tools/Pe2Lib.php
+++ src/tools/Pe2Lib.php
@@ -26,6 +26,8 @@ function getLocalPath($filePath){
 }
 
 function getPdbName($inputPath){
+	global $TOOLCHAIN;
+
 	$arg = escapeshellarg($inputPath);
 	$cmd = escapeshellarg("{$TOOLCHAIN}/dumpbin.exe");
 
@@ -52,6 +54,8 @@ function getPdbName($inputPath){
 }
 
 function downloadPdb($inputPath, $pdbName){
+	global $DBGTOOLS;
+
 	$cmd = escapeshellarg("${DBGTOOLS}/symchk.exe");
 	$pwd = getcwd();
 	if(strpos($pwd, " ") !== FALSE){
@@ -93,6 +97,42 @@ function rmrf($dir) {
 function ifcopy($src, $dst){
 	if(!file_exists($dst))
 		copy($src, $dst);
+}
+
+function array_insert_null($array, $index){
+	$keys = array_keys($array);
+	$num_keys = count($keys);
+
+	if(!is_numeric($index)){
+		$index = array_search($index, $keys);
+	}
+
+	$vals = array_values($array);
+
+
+	// preserve N fields
+	$pre = array_slice($vals, 0, $index);
+	
+	// add NULL
+	$vals = array_slice($vals, $index);
+	array_unshift($vals, NULL);
+	
+	// remove excess elements
+	$vals = array_merge($pre, $vals);
+	$num = count($vals);
+	$excess = $num - $num_keys;
+	$removed = array_splice($vals, $num - $excess - 1, $excess);
+
+	// last field is used as excess store
+	$store = end($vals);
+	$vals[key($vals)] = array_merge($removed, $store); //prepend removed elements
+	reset($vals);
+
+	return array_combine($keys, $vals);
+}
+
+function seems_hex($str){
+	return ctype_xdigit($str) && (strtoupper($str) == $str);
 }
 
 $opts = getopt("s", [], $optind);
@@ -144,6 +184,10 @@ $def = fopen($outDef, "r") or die("open fail" . PHP_EOL);
 $newdef = fopen($outBase . "_fixed.def", "w+") or die("open fail" . PHP_EOL);
 
 $found = false;
+$NO_NAME = "[NONAME]";
+
+$line_fields = array("ordinal", "hint", "rva", "name", "rest");
+
 while(!feof($def)){
 	$line = trim(fgets($def));
 	if(empty($line))
@@ -165,36 +209,60 @@ while(!feof($def)){
 			continue;
 		}
 
-		$ordinal = array_shift($parts);
-		
-		$hint = null;
-		$name = null;
-		$noname = null;
+		$lineData = array_combine($line_fields, array(
+			array_shift($parts),
+			array_shift($parts),
+			array_shift($parts),
+			array_shift($parts),
+			$parts
+		));
 
-		$last = array_pop($parts);
-		// no name (nor public nor pdb)
-		if($last == "[NONAME]"){
+		if(!is_numeric($lineData["ordinal"])){
+			print("Skipping invalid ordinal {$lineData['ordinal']}\n");
 			continue;
 		}
 
-		$hint = array_shift($parts);
-		$rva = array_shift($parts);
-		
-		switch(count($parts)){
-			// public name
-			case 0:
-				$name = $last;
-				break;
-			// name from pdb
-			default:
-				$part = array_shift($parts);
-				if($name == "[NONAME]")
-					$noname = " NONAME";
-				$name = array_shift($parts);
-				break;
+		if(
+			!seems_hex($lineData["hint"]) ||
+			strpos($lineData["hint"], "0") === 0
+		){
+			$lineData = array_insert_null($lineData, "hint");
 		}
-		
-		fwrite($newdef, "{$name} @ {$ordinal}{$noname}\r\n");
+
+		if(!seems_hex($lineData["rva"])){
+			$lineData = array_insert_null($lineData, "rva");
+		}
+
+		$hasNoName = ($lineData["name"] == $NO_NAME);
+		$mangledName = null;
+
+		if($hasNoName){
+			$rest = &$lineData["rest"];
+			$rest0 = array_shift($rest);
+
+			// name not available
+			if(is_null($rest0)){
+				continue;
+			}
+
+			if($rest0[0] == '(') //e.g "(forwarded", illegal C name anyways
+				continue;
+				
+			$lineData["name"] = $rest0;
+
+			if($dlPdb && count($rest) > 0){
+				$mangledName = implode(" ", $rest);
+				if(preg_match("/\((.*)\)/", $mangledName, $m)){
+					$mangledName = $m[1];
+				}
+			}
+		}
+
+		{
+			$noname = ($hasNoName) ? " NONAME" : "";
+			$comment = (is_null($mangledName)) ? "" : " ; {$mangledName}";
+			fwrite($newdef, "{$lineData['name']} @ {$lineData['ordinal']}{$noname}{$comment}\r\n");
+		}
 	}
 }
 fclose($def);
