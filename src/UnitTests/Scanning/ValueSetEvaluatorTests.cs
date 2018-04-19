5f23e3eb3ec4a92730c3a5704593f651d747b961
John Källén <johnkal@gmail.com>
2018-04-19 16:43:58 +0200

    Refactor ValueSetEvaluatorTests

--- src/UnitTests/Scanning/ValueSetEvaluatorTests.cs
+++ src/UnitTests/Scanning/ValueSetEvaluatorTests.cs
@@ -35,7 +35,9 @@ namespace Reko.UnitTests.Scanning
     [TestFixture]
     public class ValueSetEvaluatorTests
     {
+        private ValueSetEvaluator vse;
         private Program program;
+        private Dictionary<Expression, ValueSet> valueSets;
         private ProcedureBuilder m;
         private DataType W16;
         private DataType W32;
@@ -43,6 +45,7 @@ namespace Reko.UnitTests.Scanning
         [SetUp]
         public void Setup()
         {
+            vse = null;
             var addr = Address.Ptr32(0x2000);
             m = new ProcedureBuilder();
             W16 = PrimitiveType.Word16;
@@ -61,6 +64,38 @@ namespace Reko.UnitTests.Scanning
                         new MemoryArea(Address.SegPtr(0xC00, 0), new Byte[0x400]),
                         AccessMode.ReadWriteExecute))
             };
+
+            this.valueSets = new Dictionary<Expression, ValueSet>(new ExpressionValueComparer());
+        }
+
+        private void Given_ValueSet(Identifier exp, ValueSet valueSet)
+        {
+            valueSets.Add(exp, valueSet);
+        }
+
+        private void Given_Evaluator()
+        {
+            this.vse = new ValueSetEvaluator(
+                program,
+                valueSets);
+        }
+
+        private void Given_UInt32Array(uint uAddr, uint[] uints)
+        {
+            var w = program.CreateImageWriter(Address.Ptr32(uAddr));
+            foreach (uint u in uints)
+            {
+                w.WriteUInt32(u);
+            }
+        }
+
+        private void Given_UInt16Array(Address addr, ushort[] ushorts)
+        {
+            var w = program.CreateImageWriter(addr);
+            foreach (ushort u in ushorts)
+            {
+                w.WriteUInt16(u);
+            }
         }
 
         private ValueSet IVS(int stride, long low, long high)
@@ -81,12 +116,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Identifier()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, 0, 20) }
-                });
+            Given_ValueSet(r1, IVS(4, 0, 20));
+            Given_Evaluator();
+        
             var vs = r1.Accept(vse);
             Assert.AreEqual("4[0,14]", vs.ToString());
         }
@@ -95,12 +127,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Sum()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, 0, 20) }
-                });
+            Given_ValueSet(r1, IVS(4, 0, 20));
+            Given_Evaluator();
+
             var vs = m.IAdd(r1, 9).Accept(vse);
             Assert.AreEqual("4[9,1D]", vs.ToString());
         }
@@ -108,32 +137,25 @@ namespace Reko.UnitTests.Scanning
         [Test]
         public void Vse_Load()
         {
-            var w = program.CreateImageWriter(Address.Ptr32(0x2000));
-            w.WriteUInt32(0x3000);
-            w.WriteUInt32(0x3028);
-            w.WriteUInt32(0x3008);
-            var r1 = m.Reg32("r1", 1);
+            Given_UInt32Array(0x2000, new uint[] { 0x3000, 0x3028, 0x3008 });
 
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, 0x2000, 0x2008) }
-                });
+            var r1 = m.Reg32("r1", 1);
+            Given_ValueSet(r1, IVS(4, 0x2000, 0x2008));
+            Given_Evaluator();
+
             var vs = m.Mem32(r1).Accept(vse);
             Assert.AreEqual("[0x00003000,0x00003028,0x00003008]", vs.ToString());
         }
+
+  
 
         [Test]
         public void Vse_And()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, -4000, 4000) }
-                });
+            Given_ValueSet(r1, IVS(4, -4000, 4000));
+            Given_Evaluator();
+
             var vs = m.And(r1, 0x1F).Accept(vse);
             Assert.AreEqual("1[0,1F]", vs.ToString());
         }
@@ -142,12 +164,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Shl()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, -0x40, 0x40) }
-                });
+            Given_ValueSet(r1, IVS(4, -0x40, 0x40));
+            Given_Evaluator();
+
             var vs = m.Shl(r1, 2).Accept(vse);
             Assert.AreEqual("10[-100,100]", vs.ToString());
         }
@@ -156,12 +175,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Cast_Truncate_SingleValue()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(0, -0x43F, -0x43F) }
-                });
+            Given_ValueSet(r1, IVS(0, -0x43F, -0x43F));
+            Given_Evaluator();
+
             var vs = m.Cast(PrimitiveType.Byte, r1).Accept(vse);
             Assert.AreEqual("0[-3F,-3F]", vs.ToString());
         }
@@ -170,12 +186,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Cast_Truncate_Interval()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, -0x400, 0x400) }
-                });
+            Given_ValueSet(r1, IVS(4, -0x400, 0x400));
+            Given_Evaluator();
+
             var vs = m.Cast(PrimitiveType.Byte, r1).Accept(vse);
             Assert.AreEqual("4[0,FF]", vs.ToString());
         }
@@ -184,12 +197,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Cast_SignExtend()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, CVS(0x1FF, 0x00, 0x7F) }
-                });
+            Given_ValueSet(r1, CVS(0x1FF, 0x00, 0x7F));
+            Given_Evaluator();
+
             var vs = m.Cast(
                 PrimitiveType.Int32,
                 m.Cast(PrimitiveType.Byte, r1)).Accept(vse);
@@ -200,12 +210,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Add_self()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, IVS(4, 10, 20) }
-                });
+            Given_ValueSet(r1, IVS(4, 10, 20));
+            Given_Evaluator();
+
             var vs = m.IAdd(r1, r1).Accept(vse);
             Assert.AreEqual("8[14,28]", vs.ToString());
         }
@@ -214,12 +221,9 @@ namespace Reko.UnitTests.Scanning
         public void Vse_Add_Mul()
         {
             var r1 = m.Reg32("r1", 1);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r1, CVS(3, 9, 10) }
-                });
+            Given_ValueSet(r1, CVS(3, 9, 10));
+            Given_Evaluator();
+
             var vs = m.IMul(r1, 4).Accept(vse);
             Assert.AreEqual("[0x0000000C,0x00000024,0x00000028]", vs.ToString());
         }
@@ -228,24 +232,21 @@ namespace Reko.UnitTests.Scanning
         public void Vse_segmented_addrs()
         {
             program.Architecture = new Reko.Arch.X86.X86ArchitectureReal("x86-real-16");
-            var w = program.CreateImageWriter(Address.SegPtr(0xC00, 4));
-            w.WriteLeUInt16(0x1234);
-            w.WriteLeUInt16(0x0C00);
-            w.WriteLeUInt16(0x2468);
-            w.WriteLeUInt16(0x0C00);
-            w.WriteLeUInt16(0x369C);
-            w.WriteLeUInt16(0x0C00);
-            var r1 = m.Reg32("r1", 1);
+            Given_UInt16Array(Address.SegPtr(0xC00, 4), new ushort[] {
+                0x1234,
+                0x0C00,
+                0x2468,
+                0x0C00,
+                0x369C,
+                0x0C00,
+            });
 
             var bx = m.Reg16("bx", 3);
             var cs = m.Reg16("cs", 0x0C00);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { bx, IVS(1, 0, 2) },
-                    { cs, IVS(0, 0xC00, 0xC00) }
-                });
+            Given_ValueSet(bx, IVS(1, 0, 2));
+            Given_ValueSet(cs, IVS(0, 0xC00, 0xC00));
+            Given_Evaluator();
+
             var vs = m.SegMem(PrimitiveType.SegPtr32, cs, m.IAdd(m.Shl(bx, 2), 4)).Accept(vse);
             Assert.AreEqual("[0C00:1234,0C00:2468,0C00:369C]", vs.ToString());
         }
@@ -257,17 +258,24 @@ namespace Reko.UnitTests.Scanning
             // (int32) (int16) Mem0[(word32) (word16) ((word32) r0 * 0x00000002) + 0x0010EC32:word16] + 0x0010EC30
       
             var r0 = m.Reg32("r0", 0);
+
+            Given_ValueSet(r0, IVS(1, 0, 3));
+            Given_Evaluator();
+
             var exp = m.IAdd(
                 m.Cast(W32, m.Cast(W16, m.IMul(m.Cast(W32, r0), 2))),
                 0x00100000);
-            var vse = new ValueSetEvaluator(
-                program,
-                new Dictionary<Expression, ValueSet>(new ExpressionValueComparer())
-                {
-                    { r0, IVS(1, 0, 3) },
-                });
             var vs = exp.Accept(vse);
             Assert.AreEqual("2[100000,100006]", vs.ToString());
+        }
+
+        [Test]
+        public void Vse_GetType()
+        {
+            var r0 = m.Reg32("r0", 0);
+            var exp = r0;
+
+            Given_Evaluator();
         }
     }
 }
