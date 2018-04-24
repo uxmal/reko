982595c7d41ff52a93824d339300bf8bd53e5820
Stefano Moioli <smxdev4@gmail.com>
2018-04-24 19:22:06 +0200

    getAssemblies.bat: use a variable for toplevel

--- src/UserInterfaces/Electron/web/getAssemblies.bat
+++ src/UserInterfaces/Electron/web/getAssemblies.bat
@@ -2,11 +2,13 @@
 rem Copies .NET assemblies and other shared libraries from the Reko solution into
 rem the `generated\assemblies` folder so they are visible to the Electron application.
 
-xcopy /d /y ..\..\..\Drivers\CmdLine\bin\x64\Debug\*.dll generated\assemblies
-xcopy /d /y ..\..\..\Drivers\CmdLine\bin\x64\Debug\*.xml generated\assemblies
-xcopy /d /y ..\..\..\Drivers\CmdLine\bin\x64\Debug\*.config generated\assemblies
-xcopy /d /y ..\..\..\Drivers\WindowsDecompiler\bin\x64\Debug\Reko.Gui.dll generated\assemblies
-xcopy /d /y ..\..\Electron.Adapter\bin\Debug\Reko.Gui.Electron.Adapter.dll generated\assemblies
+set REKO=%~dp0\..\..\..
+
+xcopy /d /y %REKO%\Drivers\CmdLine\bin\x64\Debug\*.dll generated\assemblies
+xcopy /d /y %REKO%\Drivers\CmdLine\bin\x64\Debug\*.xml generated\assemblies
+xcopy /d /y %REKO%\Drivers\CmdLine\bin\x64\Debug\*.config generated\assemblies
+xcopy /d /y %REKO%\Drivers\WindowsDecompiler\bin\x64\Debug\Reko.Gui.dll generated\assemblies
+xcopy /d /y %REKO%\UserInterfaces\Electron.Adapter\bin\Debug\Reko.Gui.Electron.Adapter.dll generated\assemblies
 
 rem pause if ran by doubleclick
 echo.%cmdcmdline% | find /I "%~0" >nul
