8a41f07cdebcef08ff5e4848721bb31941d0ec75
John Källén <johnkal@gmail.com>
2018-04-25 23:53:54 +0200

    Deleted Electron UI components from `master` branch -- look in `electron-development` for these.

--- src/UserInterfaces/Electron/app/views/helpers/case.ts
+++
@@ -1,7 +0,0 @@
-//http://chrismontrois.net/2016/01/30/handlebars-switch/
-
-module.exports = function(this: any, value:any, options:any) {
-	if (value == this._switch_value_) {
-	return options.fn(this);
-	}
-};
--- src/UserInterfaces/Electron/getAssemblies.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-rem Copies .NET assemblies and other shared libraries from the Reko solution into
-rem the `generated\assemblies` folder so they are visible to the Electron application.
-
-xcopy /d /y ..\Electron.Adapter\bin\debug\Reko.Gui.Electron.Adapter.dll generated\assemblies
-xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.dll generated\assemblies
-xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.xml generated\assemblies
-xcopy /d /y ..\..\Drivers\CmdLine\bin\debug\*.config generated\assemblies
-xcopy /d /y ..\..\Drivers\WindowsDecompiler\bin\Debug\Reko.Gui.dll generated\assemblies
