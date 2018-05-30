ab75cd571ffea0627bac597891f2b9866465566b
John Källén <johnkal@gmail.com>
2018-05-30 19:23:09 +0200

    Implemented SparcArchitecture.CreateStackAccess

--- src/Arch/Sparc/SparcArchitecture.cs
+++ src/Arch/Sparc/SparcArchitecture.cs
@@ -181,7 +181,8 @@ namespace Reko.Arch.Sparc
 
         public override Expression CreateStackAccess(IStorageBinder binder, int cbOffset, DataType dataType)
         {
-            throw new NotImplementedException();
+            var sp = binder.EnsureRegister(Registers.sp);
+            return MemoryAccess.Create(sp, cbOffset, dataType);
         }
 
         public override Address MakeAddressFromConstant(Constant c)
