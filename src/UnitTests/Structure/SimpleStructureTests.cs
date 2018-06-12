7561c614f133f8ce8a5542d01aa836bea89ecc84
John Källén <johnkal@gmail.com>
2018-06-12 14:49:43 +0200

    Save test for #568

--- src/UnitTests/Structure/SimpleStructureTests.cs
+++ src/UnitTests/Structure/SimpleStructureTests.cs
@@ -349,8 +349,11 @@ word32 fn0010000C(word32 dwArg04, word32 dwArg08)
         }
 
         [Test]
+        [Ignore(Categories.FailedTests)]
         public void StrReg00568()
         {
+            // We are generating a redundant check in the complex instruction.
+            // It will probably need value set analysis to be properly resolved.
             RunTest("Fragments/regressions/r00568.asm", "Structure/StrReg00568.txt");
         }
     }
