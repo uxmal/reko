ea517a227eeedc98a8f856381699a42a3c5076c6
John Källén <johnkal@gmail.com>
2018-06-14 12:04:16 +0200

    Allow custom loader in CmdLineDriver

--- src/Decompiler/Loading/NullImageLoader.cs
+++ src/Decompiler/Loading/NullImageLoader.cs
@@ -68,6 +68,13 @@ namespace Reko.Loading
             return program;
         }
 
+        public override Program Load(Address addrLoad, IProcessorArchitecture arch, IPlatform platform)
+        {
+            this.Architecture = arch;
+            this.Platform = platform;
+            return this.Load(addrLoad);
+        }
+
         public override RelocationResults Relocate(Program program, Address addrLoad)
         {
             return new RelocationResults(EntryPoints, new SortedList<Address, ImageSymbol>());
