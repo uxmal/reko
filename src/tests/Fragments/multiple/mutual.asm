3f2beba10e8d7ba15e313c7b36ea87dc69d0bfee
John Källén <johnkal@gmail.com>
2018-09-20 12:49:49 +0200

    Clarify purpose of fragment.

--- src/tests/Fragments/multiple/mutual.asm
+++ src/tests/Fragments/multiple/mutual.asm
@@ -6,6 +6,8 @@ main	proc
 		ret
 main	endp
 
+;;; dx is live in becaused it is used in `one`
+;;; ax is trashed
 one		proc
 		cmp	dx,30
 		jnz  not_one
@@ -18,6 +20,8 @@ one_join:
 		ret
 one		endp
 
+;;; dx is live in because it is used in `two`
+;;; ax is trashed and live out.
 two		proc
 		cmp		dx,20
 		jl		not_two
