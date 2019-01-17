d906c7c15654a40f6844858f61b8913671d5577d
John Källén <johnkal@gmail.com>
2019-01-17 13:50:33 +0100

    Configuration
    Add C64.xml to Reko configuration and WiX installer.

--- src/Core/IProcessorArchitecture.cs
+++ src/Core/IProcessorArchitecture.cs
@@ -1,6 +1,6 @@
 #region License
 /* 
- * Copyright (C) 1999-2019 John Källén.
+ * Copyright (C) 1999-2019 John KÃ¤llÃ©n.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -462,7 +462,7 @@ namespace Reko.Core
 
         public virtual IEnumerable<FlagGroupStorage> GetSubFlags(FlagGroupStorage flags)
         {
-            throw new NotImplementedException("Your architecture must implement this.");
+            throw new NotImplementedException($"Your architecture must implement {nameof(GetSubFlags)}.");
         }
 
 
