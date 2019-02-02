27252a936da87c7d6ab0d02c7d8971a0a6e00c19
John Källén <johnkal@gmail.com>
2019-02-02 23:29:03 +0100

    Fix flag bit sizes for ARM.

--- src/Arch/Arm/AArch32/ArmRewriter.cs
+++ src/Arch/Arm/AArch32/ArmRewriter.cs
@@ -951,11 +951,11 @@ namespace Reko.Arch.Arm.AArch32
             //default:
             //	throw new NotImplementedException(string.Format("ARM condition code {0} not implemented.", cond));
             case ArmCondition.HS:
-                return m.Test(ConditionCode.UGE, FlagGroup(FlagM.CF, "C", PrimitiveType.Byte));
+                return m.Test(ConditionCode.UGE, FlagGroup(FlagM.CF, "C", PrimitiveType.Bool));
             case ArmCondition.LO:
-                return m.Test(ConditionCode.ULT, FlagGroup(FlagM.CF, "C", PrimitiveType.Byte));
+                return m.Test(ConditionCode.ULT, FlagGroup(FlagM.CF, "C", PrimitiveType.Bool));
             case ArmCondition.EQ:
-                return m.Test(ConditionCode.EQ, FlagGroup(FlagM.ZF, "Z", PrimitiveType.Byte));
+                return m.Test(ConditionCode.EQ, FlagGroup(FlagM.ZF, "Z", PrimitiveType.Bool));
             case ArmCondition.GE:
                 return m.Test(ConditionCode.GE, FlagGroup(FlagM.NF | FlagM.ZF | FlagM.VF, "NZV", PrimitiveType.Byte));
             case ArmCondition.GT:
@@ -969,15 +969,15 @@ namespace Reko.Arch.Arm.AArch32
             case ArmCondition.LT:
                 return m.Test(ConditionCode.LT, FlagGroup(FlagM.NF | FlagM.VF, "NV", PrimitiveType.Byte));
             case ArmCondition.MI:
-                return m.Test(ConditionCode.LT, FlagGroup(FlagM.NF, "N", PrimitiveType.Byte));
+                return m.Test(ConditionCode.LT, FlagGroup(FlagM.NF, "N", PrimitiveType.Bool));
             case ArmCondition.PL:
-                return m.Test(ConditionCode.GE, FlagGroup(FlagM.NF, "N", PrimitiveType.Byte));
+                return m.Test(ConditionCode.GE, FlagGroup(FlagM.NF, "N", PrimitiveType.Bool));
             case ArmCondition.NE:
-                return m.Test(ConditionCode.NE, FlagGroup(FlagM.ZF, "Z", PrimitiveType.Byte));
+                return m.Test(ConditionCode.NE, FlagGroup(FlagM.ZF, "Z", PrimitiveType.Bool));
             case ArmCondition.VC:
-                return m.Test(ConditionCode.NO, FlagGroup(FlagM.VF, "V", PrimitiveType.Byte));
+                return m.Test(ConditionCode.NO, FlagGroup(FlagM.VF, "V", PrimitiveType.Bool));
             case ArmCondition.VS:
-                return m.Test(ConditionCode.OV, FlagGroup(FlagM.VF, "V", PrimitiveType.Byte));
+                return m.Test(ConditionCode.OV, FlagGroup(FlagM.VF, "V", PrimitiveType.Bool));
             }
             return null;
         }
