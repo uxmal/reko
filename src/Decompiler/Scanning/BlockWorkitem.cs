0a7ae17641708688656fb0f2bcde377cc12e90ec
John Källén <johnkal@gmail.com>
2019-02-03 09:12:15 +0100

    Remove debugging code

--- src/Decompiler/Scanning/BlockWorkitem.cs
+++ src/Decompiler/Scanning/BlockWorkitem.cs
@@ -105,8 +105,6 @@ namespace Reko.Scanning
             while (rtlStream.MoveNext())
             {
                 this.ric = rtlStream.Current;
-                if (ric.Address.ToLinear() == 0x00B678)
-                    ric.ToString();
                 if (blockCur != scanner.FindContainingBlock(ric.Address))
                     break;  // Fell off the end of this block.
                 if (!ProcessRtlCluster(ric))
