uxmal/reko

commit 5733d8e7f37f175a51517405eb9b3b41619a42da

Bug: SSA transform of stack variables
Produce correct SEQuences of SLICEes.

src/UnitTests/Analysis/SsaTransformTests.cs
@@ -131,7 +131,7 @@ namespace Reko.UnitTests.Analysis
             }
             var sActual = writer.ToString();
             if (sActual != sExp)
-                Debug.Print(sActual);
+                Console.WriteLine(sActual);
             Assert.AreEqual(sExp, sActual);
         }
 
@@ -201,10 +201,10 @@ namespace Reko.UnitTests.Analysis
             var sActual = writer.ToString();
             if (sActual != sExp)
             {
-                Debug.Print("<< Expected ========");
-                Debug.Print(sExp);
-                Debug.Print(">> Actual ==========");
-                Debug.Print(sActual);
+                Console.WriteLine("<< Expected ========");
+                Console.WriteLine(sExp);
+                Console.WriteLine(">> Actual ==========");
+                Console.WriteLine(sActual);
             }
             Assert.AreEqual(sExp, sActual);
         }
@@ -240,6 +240,7 @@ namespace Reko.UnitTests.Analysis
         {
             sst.RenameFrameAccesses = true;
             sst.Transform();
+            sst.SsaState.Dump(true);
             sst.SsaState.Validate(s => Assert.Fail(s));
         }
 
@@ -257,7 +258,7 @@ namespace Reko.UnitTests.Analysis
             var actual = writer.ToString();
             if (actual != expected)
             {
-                Debug.Print(actual);
+                Console.WriteLine(actual);
             }
             Assert.AreEqual(expected, actual);
         }
@@ -2782,12 +2783,12 @@ r1_7: orig: r1
     uses: use r1_7
 wLoc02_8: orig: wLoc02
     def:  wLoc02_8 = 0x1234
-    uses: dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9) (alias)
+    uses: dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9)
 wLoc04_9: orig: wLoc04
     def:  wLoc04_9 = 0x5678
-    uses: dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9) (alias)
+    uses: dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9)
 dwLoc04_10: orig: dwLoc04
-    def:  dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9) (alias)
+    def:  dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9)
     uses: r1_7 = dwLoc04_10
 // proc1
 // Return size: 0
@@ -2801,7 +2802,7 @@ l1:
 	wLoc02_8 = 0x1234
 	r63_5 = fp - 0x00000004
 	wLoc04_9 = 0x5678
-	dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9) (alias)
+	dwLoc04_10 = SEQ(wLoc02_8, wLoc04_9)
 	r1_7 = dwLoc04_10
 	return
 	// succ:  proc1_exit
@@ -3515,10 +3516,11 @@ main_exit:
 	def fp
 body:
 	qwLoc08_6 = 0x0000000000005678
+	dwLoc04_9 = SLICE(qwLoc08_6, word32, 32) (alias)
 	dwLoc08_7 = 0x00001234
 	dwLoc08_8 = dwLoc08_7 + 0x00000001
-	qwLoc08_9 = DPB(qwLoc08_6, dwLoc08_8, 0) (alias)
-	Mem5[0x0000567C:word64] = qwLoc08_9
+	qwLoc08_10 = SEQ(dwLoc04_9, dwLoc08_8)
+	Mem5[0x0000567C:word64] = qwLoc08_10
 	return
 proc_exit:
 ";
@@ -3615,7 +3617,6 @@ proc_exit:
 ";
             #endregion
             AssertProcedureCode(expected);
-
         }
 
         [Test]
@@ -3712,7 +3713,7 @@ l1:
 	dwLoc08_13 = r1_7
 	r1_9 = Mem8[0x00123408:word32]
 	dwLoc04_14 = r1_9
-	nLoc0C_15 = SEQ(dwLoc0C_12, dwLoc08_13, dwLoc04_14) (alias)
+	nLoc0C_15 = SEQ(dwLoc0C_12, dwLoc08_13, dwLoc04_14)
 	fp0_11 = nLoc0C_15
 	return
 Ssa96BitStackLocal_exit:

src/Decompiler/Analysis/SsaMutator.cs
@@ -73,7 +73,7 @@ namespace Reko.Analysis
         /// <param name="register">The register whose post-call value is incremented.</param>
         /// <param name="delta">The amount by which to increment the register.</param>
         public void AdjustRegisterAfterCall(
-                        Statement stm,
+            Statement stm,
             CallInstruction call,
             RegisterStorage register,
             int delta)

src/Decompiler/Analysis/SsaTransform.cs
@@ -37,7 +37,7 @@ namespace Reko.Analysis
     /// form.
 	/// </summary>
     /// <remarks>
-    /// This class implements an SSA algorithm that doesn't require 
+    /// This class implements an SSA algorithm that does not require the
     /// calculation of the dominator graph. It is based on the algorithm
     /// described in "Simple and Efficient Construction of Static Single
     /// Assignment Form" by Matthias Braun, Sebastian Buchwald, Sebastian 
@@ -51,19 +51,19 @@ namespace Reko.Analysis
     {
         private static TraceSwitch trace = new TraceSwitch("SsaTransform", "Traces the progress of SSA analysis") { Level = TraceLevel.Info };
 
-        private IProcessorArchitecture arch;
-        private Program program;
-        private ProgramDataFlow programFlow;
-        private IImportResolver importResolver;
+        private readonly IProcessorArchitecture arch;
+        private readonly Program program;
+        private readonly ProgramDataFlow programFlow;
+        private readonly IImportResolver importResolver;
+        private readonly Dictionary<Block, SsaBlockState> blockstates;
+        private readonly SsaState ssa;
+        private readonly TransformerFactory factory;
+        public readonly HashSet<SsaIdentifier> incompletePhis;
+        private readonly HashSet<Procedure> sccProcs;
+        private readonly ExpressionEmitter m;
+        private HashSet<SsaIdentifier> sidsToRemove;
         private Block block;
         private Statement stmCur;
-        private Dictionary<Block, SsaBlockState> blockstates;
-        private SsaState ssa;
-        private TransformerFactory factory;
-        public readonly HashSet<SsaIdentifier> incompletePhis;
-        private HashSet<SsaIdentifier> sidsToRemove;
-        private HashSet<Procedure> sccProcs;
-        private ExpressionEmitter m;
 
         public SsaTransform(
             Program program,
@@ -72,7 +72,7 @@ namespace Reko.Analysis
             IImportResolver importResolver,
             ProgramDataFlow programFlow)
         {
-            this.arch = program.Architecture;
+            this.arch = proc.Architecture;
             this.program = program;
             this.programFlow = programFlow;
             this.importResolver = importResolver;
@@ -1039,7 +1039,7 @@ namespace Reko.Analysis
 
         public class TransformerFactory : StorageVisitor<IdentifierTransformer>
         {
-            private SsaTransform transform;
+            private readonly SsaTransform transform;
             private Identifier id;
             private Statement stm;
 
@@ -1263,6 +1263,8 @@ namespace Reko.Analysis
                     sidUse = ReadVariableRecursive(bsTo);
                     e = new DepositBits(sidUse.Identifier, aliasFrom.SsaId.Identifier, (int)stgFrom.BitAddress);
                 }
+
+
                 var ass = new AliasAssignment(id, e);
                 var sidAlias = InsertAfterDefinition(sidFrom.DefStatement, ass);
                 sidUse.Uses.Add(sidAlias.DefStatement);
@@ -1272,6 +1274,20 @@ namespace Reko.Analysis
                 return sidAlias;
             }
 
+            /// <summary>
+            /// Inserts the assignment <paramref name="ass"/> before the statement
+            /// <paramref name="stm"/>.
+            /// </summary>
+            public SsaIdentifier InsertBeforeDefinition(Statement stm, Assignment ass)
+            {
+                int i = stm.Block.Statements.IndexOf(stm);
+                var stmBefore = new Statement(stm.LinearAddress, ass, stm.Block);
+                stm.Block.Statements.Insert(i, stmBefore);
+
+                var sidTo = ssaIds.Add(ass.Dst, stmBefore, ass.Src, false);
+                ass.Dst = sidTo.Identifier;
+                return sidTo;
+            }
 
             /// <summary>
             /// Inserts the statement <paramref name="ass"/> after the statement
@@ -1581,7 +1597,7 @@ namespace Reko.Analysis
 
         public class FlagGroupTransformer : IdentifierTransformer
         {
-            private uint flagMask;
+            private readonly uint flagMask;
             private FlagGroupStorage flagGroup;
 
             public FlagGroupTransformer(Identifier id, FlagGroupStorage flagGroup, Statement stm, SsaTransform outer)
@@ -1733,104 +1749,93 @@ namespace Reko.Analysis
             public override SsaIdentifier ReadBlockLocalVariable(SsaBlockState bs)
             {
                 var ints = bs.currentStackDef.GetIntervalsOverlappingWith(offsetInterval)
-                    .OrderByDescending(i => i.Key.End - i.Key.Start)
-                    .ThenBy(i => i.Key.Start)
+                    .OrderBy(i => i.Key.Start)
+                    .ThenBy(i => i.Key.End - i.Key.Start)
                     .Select(SliceAndShift)
                     .ToArray();
                 if (ints.Length == 0)
                     return null;
-                // Defined locally in this block.
-                if (ints.Length == 1)
+                // Part of 'id' is defined locally in this block. We now 
+                // walk across the bits of 'id'.
+                int offsetLo = this.offsetInterval.Start;
+                int offsetHi = this.offsetInterval.End;
+                var sequence = new List<(SsaIdentifier sid, Interval<int> interval)>();
+                foreach (var (sid, expr, intFrom) in ints)
                 {
-                    var stg = (StackStorage) ints[0].Item1.Identifier.Storage;
-                    var start = stg.StackOffset;
-                    var end = start + stg.DataType.Size;
-                    if (start == ints[0].Item3.Start &&
-                        end == ints[0].Item3.End)
+                    if (offsetLo < intFrom.Start)
                     {
-                        // Exact match
-                        return ints[0].Item1;
+                        // Found a gap in the interval tree that isn't defined
+                        // in this block. Look for the gap in another block.
+                        this.offsetInterval = Interval.Create(offsetLo, intFrom.Start);
+                        var sidR = ReadVariableRecursive(bs);
+                        sequence.Add((sidR, offsetInterval));
                     }
-                    return CreateSliceStatement(ints[0].Item1, stg, bs);
+                    if (offsetLo < intFrom.End)
+                    {
+                        var subInt = new Interval<int>(offsetLo, Math.Min(intFrom.End, offsetHi));
+                        sequence.Add((sid, subInt));
+                        offsetLo = intFrom.End;
+                    }
+                }
+                if (offsetLo < offsetHi)
+                {
+                    this.offsetInterval = Interval.Create(offsetLo, offsetHi);
+                    var sidR = ReadVariableRecursive(bs);
+                    sequence.Add((sidR, offsetInterval));
+                }
+                if (sequence.Count == 1)
+                {
+                    var sidTo = MakeSequenceElement(bs, sequence[0]);
+                    return sidTo;
                 }
                 else
                 {
-                    var prev = ints[0].Item3;
-                    var sequence = new List<SsaIdentifier> { ints[0].Item1 };
-                    foreach (var src in ints.Skip(1))
+                    var seq = outer.arch.Endianness.MakeSequence(this.id.DataType, sequence.Select(e => (Expression)MakeSequenceElement(bs, e).Identifier).ToArray());
+                    var assSeq = new Assignment(id, seq);
+                    var sidTo = InsertBeforeDefinition(this.stm, assSeq);
+                    foreach (Identifier item in seq.Expressions)
                     {
-                        DebugEx.PrintIf(SsaTransform.trace.TraceVerbose, "Analyze: {0} {1} {2}", src.Item1, src.Item2, src.Item3);
-                        if (prev.End == src.Item3.Start)
-                        {
-                            // Previous item ended where next starts:
-                            // extend it and create a sequence.
-                            prev = Interval.Create(prev.Start, src.Item3.End);
-                            sequence.Add(src.Item1);
-                        }
-                        else if (prev.End < src.Item3.Start)
-                        {
-                            // Gap betweeen prev item and this item.
-                            // Emit the sequence somehow.
-                            throw new NotImplementedException("do something with slice");
-                        }
-                        else if (src.Item3.Covers(prev))
-                        {
-                            sequence.Clear();
-                            sequence.Add(src.Item1);
-                            prev = src.Item3;
-                        }
-                        else if (sequence.Count > 0 && src.Item3.OverlapsWith(prev))
-                        {
-                            var dpb = new DepositBits(sequence[0].Identifier, src.Item2, src.Item3.Start - prev.Start);
-                            var ass = new AliasAssignment(id, dpb);
-                            var iStm = outer.stmCur.Block.Statements.IndexOf(outer.stmCur);
-                            var stm = outer.stmCur.Block.Statements.Insert(iStm, outer.stmCur.LinearAddress, ass);
-                            var sidTo = ssaIds.Add(ass.Dst, stm, ass.Src, false);
-                            ass.Dst = sidTo.Identifier;
-                            sequence[0].Uses.Add(stm);
-                            src.Item1.Uses.Add(stm);
-                            sequence = new List<SsaIdentifier> { sidTo };
-                        }
-                        else
-                        {
-                            prev = Interval.Create(prev.Start, src.Item3.End);
-                            sequence.Add(src.Item1);
-                        }
+                        outer.ssa.Identifiers[item].Uses.Add(sidTo.DefStatement);
                     }
-                    if (sequence.Count == 1)
-                        return sequence[0];
-                    if (sequence.Count > 1)
-                    {
-                        var seq = outer.arch.Endianness.MakeSequence(this.id.DataType, sequence.Select(s => s.Identifier).ToArray());
-                        var ass = new AliasAssignment(id, seq);
-                        var iStm = outer.stmCur.Block.Statements.IndexOf(outer.stmCur);
-                        var stm = outer.stmCur.Block.Statements.Insert(iStm, outer.stmCur.LinearAddress, ass);
-                        var sidTo = ssaIds.Add(ass.Dst, stm, ass.Src, false);
-                        ass.Dst = sidTo.Identifier;
-                        foreach (var sid in sequence)
-                        {
-                            sid.Uses.Add(stm);
-                        }
-                        return sidTo;
-                    }
+                    return sidTo;
                 }
-                return null;
             }
 
-            private SsaIdentifier CreateSliceStatement(SsaIdentifier sidFrom, StackStorage stgFrom, SsaBlockState bsTo)
+            private SsaIdentifier MakeSequenceElement(SsaBlockState bs, (SsaIdentifier sid, Interval<int> interval) elem)
             {
-                // Defined identifer is "wider" than the storage
-                // being read. The reader gets a slice of the 
-                // defined identifier.
-                int offset = stgFrom.OffsetOf(this.id.Storage);
-                var e = this.outer.arch.Endianness.MakeSlice(id.DataType, sidFrom.Identifier, offset);
+                var stg = (StackStorage) elem.sid.Identifier.Storage;
+                var start = stg.StackOffset;
+                var end = start + stg.DataType.Size;
+                if (start == elem.interval.Start &&
+                    end == elem.interval.End)
+                {
+                    // Exact match
+                    return elem.sid;
+                }
+                var sidSlice = CreateSliceStatement(elem.sid, elem.interval, elem.interval.Start - start, bs);
+                return sidSlice;
+            }
+
+            /// <summary>
+            /// Create a slice statement to extract a subinterval from 
+            /// <paramref name="sidFrom"/>.
+            /// </summary>
+            /// <remarks>
+            /// The source, or defined identifer is "wider" than the destinatior or
+            /// used storage. We must provide a slice of the defined identifier.
+            /// </remarks>
+            private SsaIdentifier CreateSliceStatement(SsaIdentifier sidFrom, Interval<int> intv, int offset, SsaBlockState bs)
+            {
+                var bitSize = (intv.End - intv.Start) * DataType.BitsPerByte;
+                var bitOffset = offset * DataType.BitsPerByte;
+                var idSlice = outer.ssa.Procedure.Frame.EnsureStackVariable(intv.Start, PrimitiveType.CreateWord(bitSize));
+                var e = this.outer.arch.Endianness.MakeSlice(idSlice.DataType, sidFrom.Identifier, bitOffset);
                 var sidUse = sidFrom;
 
-                var ass = new AliasAssignment(id, e);
+                //$TODO: perhaps this alias has already been computed?
+                var ass = new AliasAssignment(idSlice, e);
                 var sidAlias = InsertAfterDefinition(sidFrom.DefStatement, ass);
                 sidUse.Uses.Add(sidAlias.DefStatement);
-                //if (e is DepositBits)
-                //    sidFrom.Uses.Add(sidAlias.DefStatement);
                 return sidAlias;
             }
 

src/Decompiler/Analysis/UnalignedMemoryAccessFuser.cs
@@ -247,8 +247,7 @@ namespace Reko.Analysis
                 }
                 else
                 {
-                    var ass = stm.Instruction as Assignment;
-                    if (ass == null)
+                    if (!(stm.Instruction is Assignment ass))
                         continue;
                     src = ass.Src;
                 }
@@ -332,18 +331,17 @@ namespace Reko.Analysis
 
         private Tuple<string, Application> MatchIntrinsicApplication(Expression e, string [] names)
         {
-            var app = e as Application;
-            if (app == null)
+            if (e is Application app &&
+                app.Procedure is ProcedureConstant pc &&
+                pc.Procedure is PseudoProcedure intrinsic &&
+                names.Contains(intrinsic.Name))
+            {
+                return Tuple.Create(intrinsic.Name, app);
+            }
+            else
+            {
                 return null;
-            var pc = app.Procedure as ProcedureConstant;
-            if (pc == null)
-                return null;
-            var ppp = pc.Procedure as PseudoProcedure;
-            if (ppp == null)
-                return null;
-            if (!names.Contains(ppp.Name))
-                return null;
-            return Tuple.Create(ppp.Name, app);
+            }
         }
     }
 }

src/UnitTests/Analysis/UnalignedMemoryAccessFuserTests.cs
@@ -25,6 +25,7 @@ using Reko.Core;
 using Reko.Core.Expressions;
 using Reko.Core.Types;
 using Reko.UnitTests.Mocks;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -35,7 +36,7 @@ namespace Reko.UnitTests.Analysis
     public class UnalignedMemoryAccessFuserTests : AnalysisTestBase
     {
         private ProcedureBuilder m;
-        private IProcessorArchitecture arch;
+        private Mock<IProcessorArchitecture> arch;
         private IImportResolver importResolver;
         private FakeDecompilerEventListener listener;
         private Program program;
@@ -43,7 +44,7 @@ namespace Reko.UnitTests.Analysis
         [SetUp]
         public void Setup()
         {
-            arch = new Mock<IProcessorArchitecture>().Object;
+            arch = new Mock<IProcessorArchitecture>();
             importResolver = new Mock<IImportResolver>().Object;
             listener = new FakeDecompilerEventListener();
             m = new ProcedureBuilder();
@@ -74,7 +75,7 @@ namespace Reko.UnitTests.Analysis
             var sActual = sw.ToString();
             if (sExp != sActual)
             {
-                Debug.Print("{0}", sActual);
+                Console.WriteLine("{0}", sActual);
                 Assert.AreEqual(sExp, sActual);
             }
         }
@@ -555,7 +556,6 @@ ProcedureBuilder_exit:
             AssertStringsEqual(sExp, ssa);
         }
 
-
         [Test]
         public void Ufuser_Store_Bigendian()
         {
@@ -566,6 +566,7 @@ ProcedureBuilder_exit:
 
             __swl(loc40, r8);
             __swr(loc3D, r8);
+            arch.Setup(a => a.Endianness).Returns(EndianServices.Big);
             var ssa = RunTest(m);
             var sExp =
             #region Expected
@@ -576,9 +577,19 @@ r8:r8
     uses: loc40_3 = r8
 loc40_3: orig: loc40
     def:  loc40_3 = __swl(loc40, r8)
+    uses: bLoc3D_5 = SLICE(loc40_3, byte, 24) (alias)
 loc3D:Local -003D
     def:  def loc3D
-loc3D_5: orig: loc3D
+    uses: nLoc3C_6 = SLICE(loc3D, word24, 8) (alias)
+bLoc3D_5: orig: bLoc3D
+    def:  bLoc3D_5 = SLICE(loc40_3, byte, 24) (alias)
+    uses: loc3D_7 = SEQ(nLoc3C_6, bLoc3D_5)
+nLoc3C_6: orig: nLoc3C
+    def:  nLoc3C_6 = SLICE(loc3D, word24, 8) (alias)
+    uses: loc3D_7 = SEQ(nLoc3C_6, bLoc3D_5)
+loc3D_7: orig: loc3D
+    def:  loc3D_7 = SEQ(nLoc3C_6, bLoc3D_5)
+loc3D_8: orig: loc3D
     def:  loc40_3 = r8
 // ProcedureBuilder
 // Return size: 0
@@ -587,8 +598,11 @@ ProcedureBuilder_entry:
 	def loc40
 	def r8
 	def loc3D
+	nLoc3C_6 = SLICE(loc3D, word24, 8) (alias)
 	// succ:  l1
 l1:
+	bLoc3D_5 = SLICE(loc40_3, byte, 24) (alias)
+	loc3D_7 = SEQ(nLoc3C_6, bLoc3D_5)
 	loc40_3 = r8
 ProcedureBuilder_exit:
 ";

subjects/PE/m68k/hello_m68k.globals.c
@@ -1,0 +1,15 @@
+// hello_m68k.c
+// Generated by decompiling hello_m68k.exe
+// using Reko decompiler version 0.8.0.2.
+
+#include "hello_m68k.h"
+
+Eq_57 g_t0000;
+word16 g_w0AF0;
+word16 g_a1758[] = 
+	{
+	};
+word16 g_a2C58[] = 
+	{
+	};
+

commit 8ee8a56c1e4bcaa7e72256ab54b3544457d511f4

Create unit test with overlapped stack intervals to reproduce regresion
Generation of SLICEs and SEQuences for stack identifiers caused
regression since 5733d8e

Expected:

qwLoc08_6 = 0x1234567800000000
Mem3[0x000000AB:word64] = qwLoc08_6
dwLoc04_7 = 0x12340000
Mem5[0x000000CD:word32] = dwLoc04_7

but was:

qwLoc08_6 = 0x1234567800000000
dwLoc04_8 = SLICE(qwLoc08_6, word32, 32) (alias)
Mem3[0x000000AB:word64] = qwLoc08_6
dwLoc04_7 = 0x12340000
Mem5[0x000000CD:word32] = dwLoc04_8

src/UnitTests/Analysis/SsaTransformTests.cs
@@ -3758,6 +3758,39 @@ SsaDpb_exit:
             AssertProcedureCode(expected);
         }
 
+        [Test]
+        [Category(Categories.FailedTests)]
+        public void SsaOverlappedStackIntervals()
+        {
+            var proc = Given_Procedure(nameof(SsaOverlappedStackIntervals), m =>
+            {
+                var fp = m.Frame.FramePointer;
+                m.MStore(m.ISubS(fp, 8), m.Word64(0x1234567800000000));
+                m.MStore(m.Word32(0xAB), m.Mem64(m.ISubS(fp, 8)));
+                m.MStore(m.ISubS(fp, 4), m.Word32(0x12340000));
+                m.MStore(m.Word32(0xCD), m.Mem32(m.ISubS(fp, 4)));
+                m.Return();
+            });
+
+            When_RunSsaTransform();
+            When_RenameFrameAccesses();
+
+            var expected =
+            #region Expected
+@"SsaOverlappedStackIntervals_entry:
+	def fp
+l1:
+	qwLoc08_6 = 0x1234567800000000
+	Mem3[0x000000AB:word64] = qwLoc08_6
+	dwLoc04_7 = 0x12340000
+	Mem5[0x000000CD:word32] = dwLoc04_7
+	return
+SsaOverlappedStackIntervals_exit:
+";
+            #endregion
+            AssertProcedureCode(expected);
+        }
+
         private void MakeEndiannessCheck(ProcedureBuilder m)
         {
             var fp = m.Frame.FramePointer;