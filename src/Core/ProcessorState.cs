ffe131413dec8194bc36e97f7da98fdd866edfe0
John Källén <johnkal@gmail.com>
2019-04-09 14:06:55 +0200

    Issue found by @smx-smx

--- src/Core/ProcessorState.cs
+++ src/Core/ProcessorState.cs
@@ -1,6 +1,6 @@
 #region License
 /* 
- * Copyright (C) 1999-2019 John Källén.
+ * Copyright (C) 1999-2019 John KÃ¤llÃ©n.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -246,6 +246,7 @@ namespace Reko.Core
                     binVal.Left is Identifier &&
                     binVal.Right is Constant)
                 {
+                    SetRegister(reg, Constant.Invalid);
                     linearDerived[reg] = binVal;
                     return Constant.Invalid;
                 }
