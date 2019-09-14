e157c61bcc1159691d8150032556f20e7c66eca8
John Källén <johnkal@gmail.com>
2019-09-14 02:09:47 +0200

    Ran regression tests.

--- subjects/regressions/reko-121/m68k_jump.c
+++ subjects/regressions/reko-121/m68k_jump.c
@@ -4,3 +4,38 @@
 
 #include "m68k_jump.h"
 
+// 0000C02A: void fn0000C02A(Register (ptr32 Eq_n) a0, Register (ptr32 word32) a1)
+void fn0000C02A(Eq_n * a0, word32 * a1)
+{
+	globals->dwFF0F08 = a0->dw0000;
+	globals->dwFF0F04 = 0x00;
+	word32 * a6_n = a1;
+	int32 a4_n = 0x00;
+	while (globals->dwFF0F04 - globals->dwFF0F08 < 0x00)
+	{
+		ui32 d3_n = SEQ(SLICE(a4_n, word24, 8), a0->a0004[a4_n]);
+		uipr32 d0_n = SEQ(SLICE(d3_n & 0xFF, word16, 16), (word16) (d3_n & 0xFF) & 0xF0);
+		uip32 d0_n = SEQ(SLICE(d0_n, word24, 8), (byte) d0_n >> 0x02);
+		++a4_n;
+		if ((word16) d0_n <= 44)
+		{
+			switch ((word16) d0_n)
+			{
+			case 0x00:
+				ui32 d3_n = (d3_n & 0xFF & 0x0F) + 0x01;
+				while (true)
+				{
+					word16 v36_n = (word16) d3_n;
+					d3_n = SEQ(SLICE(d3_n, word16, 16), v36_n - 0x01);
+					if (v36_n == 0x00)
+						break;
+					*a6_n = 0x00;
+					globals->dwFF0F04 += 0x04;
+					++a6_n;
+				}
+				break;
+			}
+		}
+	}
+}
+
