d3da98b5a7e4852c4260a3251b4b33297c9788b0
John Källén <johnkal@gmail.com>
2020-01-22 13:25:30 +0100

    Feature: add v850 support to Reko

---
+++ src/Arch/V850/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+using Reko;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("v850 support for Reko decompiler")]
+[assembly: AssemblyDescription("v850 support for Reko decompiler")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AssemblyMetadata.Company)]
+[assembly: AssemblyProduct(AssemblyMetadata.Product)]
+[assembly: AssemblyCopyright(AssemblyMetadata.Copyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("91a2f27f-4f93-4ccb-b2f2-f4eca8e444cb")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion(AssemblyMetadata.AssemblyVersion)]
+[assembly: AssemblyFileVersion(AssemblyMetadata.AssemblyFileVersion)]
