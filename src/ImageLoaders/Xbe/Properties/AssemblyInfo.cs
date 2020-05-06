8038d026f4a3e489909af7180ef5480b177872f7
John Källén <johnkal@gmail.com>
2020-05-06 21:14:43 +0200

    Feature: initial scaffolding for Xbox XBE executables.

---
+++ src/ImageLoaders/Xbe/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+using Reko;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Xbe image loader")]
+[assembly: AssemblyDescription("Support for loading Xbox XBE image files")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AssemblyMetadata.Company)]
+[assembly: AssemblyProduct(AssemblyMetadata.Product)]
+[assembly: AssemblyCopyright(AssemblyMetadata.Copyright)]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4A801F37-BB04-43F9-A59E-D8480BB756DC")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion(AssemblyMetadata.AssemblyVersion)]
+[assembly: AssemblyFileVersion(AssemblyMetadata.AssemblyFileVersion)]
