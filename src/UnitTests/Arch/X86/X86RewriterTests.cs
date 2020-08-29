4ddbf374aa5ff9c502bf4fabe82eb388de375759
Pavel Tomin <_xyz_@mail.ru>
2020-08-29 18:05:06 +0500

    Fix broken 'X86rw_fpu_load' test

--- src/UnitTests/Arch/X86/X86RewriterTests.cs
+++ src/UnitTests/Arch/X86/X86RewriterTests.cs
@@ -1701,7 +1701,7 @@ namespace Reko.UnitTests.Arch.X86
             Run64bitTest(0xD8, 0x0D, 0x89, 0x9F, 0x00, 0x00);
             AssertCode(     // fmul
               "0|L--|0000000140000000(6): 1 instructions",
-              "1|L--|ST[Top:real64] = ST[Top:real64] * CONVERT(Mem0[0x0000000140009F8F<p64>:real32], real32, real64) ");
+              "1|L--|ST[Top:real64] = ST[Top:real64] * CONVERT(Mem0[0x0000000140009F8F<p64>:real32], real32, real64)");
         }
 
         [Test]
