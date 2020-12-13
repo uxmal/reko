aac0a49e248608fd04bae799d52a4c800a676abe
Stefano Moioli <smxdev4@gmail.com>
2020-12-13 15:00:50 +0100

    Generalized artifacts (.zip) generation Adopted and adapted FindSevenZip.cmake

---
+++ cmake/FindSevenZip.cmake
@@ -0,0 +1,36 @@
+# This file is part of the Spring engine (GPL v2 or later), see LICENSE.html
+
+# - Find 7zip
+# Find the native 7zip binary
+#
+# NOTE: We can not use 7ZIP* or 7zip* as var name, cause in CMake,
+#       var names should not start with numbers.
+#
+#  SEVENZIP_BIN   - will be set to the 7zip executable (eg. 7z.exe)
+#  SEVENZIP_FOUND - TRUE if 7zip was found
+
+INCLUDE(FindPackageHandleStandardArgs)
+
+IF    (SEVENZIP_BIN)
+	# Already in cache, be silent
+	SET(SevenZip_FIND_QUIETLY TRUE)
+ENDIF (SEVENZIP_BIN)
+
+file(TO_CMAKE_PATH "$ENV{PROGRAMFILES}" _programfiles)
+set(_PROGRAMFILESX86 "PROGRAMFILES(x86)") 
+file(TO_CMAKE_PATH "$ENV{${_PROGRAMFILESX86}}" _programfiles_x86)
+file(TO_CMAKE_PATH "$ENV{ProgramW6432}" _programw6432)
+
+# 7zr(.exe) only supports 7z archives, while 7z(.exe) and 7za(.exe)
+# additionally support many other formats (eg zip)
+find_program(SEVENZIP_BIN
+	NAMES 7z 7za
+	HINTS "${MINGWDIR}" "${MINGWLIBS}/bin" "${_programfiles}/7-zip" "${_programfiles_x86}/7-zip" "${_programw6432}/7-zip"
+	PATH_SUFFIXES bin
+	DOC "7zip executable"
+)
+# handle the QUIETLY and REQUIRED arguments and set SEVENZIP_FOUND to TRUE if
+# all listed variables are TRUE
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(SevenZip DEFAULT_MSG SEVENZIP_BIN)
+
+MARK_AS_ADVANCED(SEVENZIP_BIN)
