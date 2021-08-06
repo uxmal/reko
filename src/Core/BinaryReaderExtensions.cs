56de0305ddb941c428b4525e4f3f7b5b80a43f73
John Källén <johnkal@gmail.com>
2021-08-06 17:07:01 +0200

    Performance: micro-optimization

--- src/Core/BinaryReaderExtensions.cs
+++ src/Core/BinaryReaderExtensions.cs
@@ -1,21 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
+#region License
+/* 
+ * Copyright (C) 1999-2021 John Källén.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; see the file COPYING.  If not, write to
+ * the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#endregion 
+
 using System.IO;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Reko.Core
 {
-	public static class BinaryReaderExtensions
+    public static class BinaryReaderExtensions
 	{
 		public static string ReadNullTerminatedString(this BinaryReader rdr)
 		{
-			string str = "";
-			char ch;
-			while ((int)(ch = rdr.ReadChar()) != 0)
-				str = str + ch;
-			return str;
+			var sb = new StringBuilder();
+            for (; ;)
+            {
+                int ch = rdr.ReadChar();
+                if (ch <= 0)
+                    return sb.ToString();
+                sb.Append((char)ch);
+            }
 		}
 	}
 }
