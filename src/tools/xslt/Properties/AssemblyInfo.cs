2ad97b419e170eb5d7cdef28c32edb0e615df268
John Källén <johnkal@gmail.com>
2021-08-16 12:08:36 +0200

    Maintenance: bumped to version 0.10.0

--- src/tools/xslt/Properties/AssemblyInfo.cs
+++ src/tools/xslt/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("0.9.4.0")]
-[assembly: AssemblyFileVersion("0.9.4.0")]
+[assembly: AssemblyVersion("0.10.0.0")]
+[assembly: AssemblyFileVersion("0.10.0.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
