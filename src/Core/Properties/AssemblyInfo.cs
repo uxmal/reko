72e64cab9401ae56d45c0886df7aa52d7b62532e
John Källén <johnkal@gmail.com>
2021-08-16 21:38:53 +0200

    Maintenance: bump version

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -42,8 +42,8 @@ namespace Reko
 {
     public static class AssemblyMetadata
     {
-        public const string AssemblyVersion = "0.10.0.0";
-        public const string AssemblyFileVersion = "0.10.0.0";
+        public const string AssemblyVersion = "0.10.1.0";
+        public const string AssemblyFileVersion = "0.10.1.0";
         public const string Product = "Reko decompiler";
         public const string Copyright = "Copyright © 1999-2021 John Källén";
         public const string Company = "John Källén Konsult AB";
