5cc781fa47bd2f207b87ff4dc9d6ebfc408bc2ef
Stefano Moioli <smxdev4@gmail.com>
2021-12-11 20:37:21 +0100

    Try using ARM64 instead of arm64

--- src/tools/regressionTests/Program.cs
+++ src/tools/regressionTests/Program.cs
@@ -97,11 +97,6 @@ namespace Reko.Tools.regressionTests
 
         private string DetermineDecompilerPath()
         {
-            string platform = this.platform;
-            if(platform == "arm64"){
-                platform = "ARM64";
-            }
-
             string reko_cmdline_dir = Path.GetFullPath(Path.Combine(reko_src, "Drivers", "CmdLine"));
             string exeFileName = DecompilerExecutableName;
             if(File.Exists(exeFileName + ".exe"))
@@ -114,7 +109,7 @@ namespace Reko.Tools.regressionTests
             }
             var s = Path.Combine(
                 reko_cmdline_dir, "bin",
-                platform, this.configuration, this.framework,
+                this.platform, this.configuration, this.framework,
                 exeFileName);
             return s;
         }
