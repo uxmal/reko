5c7948714e678ccb6116188c07adfe3e92aaf891
John Källén <johnkal@gmail.com>
2022-02-22 22:53:42 +0100

    Maintenance: bump version number to 0.11.0
    The change to .NET 5.0 causes a change in the versioning.

--- src/Core/Properties/AssemblyInfo.cs
+++ src/Core/Properties/AssemblyInfo.cs
@@ -42,8 +42,8 @@ namespace Reko
 {
     public static class AssemblyMetadata
     {
-        public const string AssemblyVersion = "0.10.2.0";
-        public const string AssemblyFileVersion = "0.10.2.0";
+        public const string AssemblyVersion = "0.11.0.0";
+        public const string AssemblyFileVersion = "0.11.0.0";
         public const string Product = "Reko decompiler";
         public const string Copyright = "Copyright © 1999-2022 John Källén";
         public const string Company = "John Källén Konsult AB";
--- src/tools/xslt/Properties/AssemblyInfo.cs
+++ src/tools/xslt/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
 
-[assembly: AssemblyVersion("0.10.2.0")]
-[assembly: AssemblyFileVersion("0.10.2.0")]
+[assembly: AssemblyVersion("0.11.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
 [assembly: AssemblyDelaySign(false)]
 [assembly: AssemblyKeyFile("")]
 [assembly: AssemblyKeyName("")]
