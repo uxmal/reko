90abd9b491c2052806296eed828d675c6a5b3659
Stefano Moioli <smxdev4@gmail.com>
2022-12-09 17:41:32 +0100

    New CMake-based git hash generator with caching Avoids regeneraring files (and triggering a rebuild) if the git hash didn't change

--- src/reko.cmake
+++ src/reko.cmake
@@ -3,15 +3,26 @@ list(INSERT CMAKE_MODULE_PATH 0 "${REKO_SRC}/../cmake")
 
 include(msbuild2cmake)
 
+set(cmake_arguments "")
+list(APPEND cmake_arguments
+	-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
+	-DREKO_SRC=${REKO_SRC}
+	-DREKO_PLATFORM=${REKO_PLATFORM}
+	-DTARGET=${TARGET}
+)
+
+if("${TARGET}" STREQUAL "git_hash")
+	list(APPEND cmake_arguments
+		-DGIT_HASH_OUTPUT=${GIT_HASH_OUTPUT}
+	)
+endif()
+
+
 invoke_cmake(
 	BUILD_DIR ${CMAKE_BINARY_DIR}/build/${TARGET}
 	DIRECTORY ${REKO_SRC}/../
 	TARGET ${TARGET}
 	GENERATOR ${REKO_COMPILER}
 	# variables needed by CMakeLists.txt, that must be forwarded
-	EXTRA_ARGUMENTS
-		-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-		-DREKO_SRC=${REKO_SRC}
-		-DREKO_PLATFORM=${REKO_PLATFORM}
-		-DTARGET=${TARGET}
+	EXTRA_ARGUMENTS ${cmake_arguments}	
 )
