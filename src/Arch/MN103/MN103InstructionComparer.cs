fdf289ddb8946887819fbd431782c465d42b59d6
John Källén <johnkal@gmail.com>
2026-09-08 01:28:38 +0200

    Feature: MN103InstructionComparer implementation.

--- src/Arch/MN103/MN103InstructionComparer.cs
+++ src/Arch/MN103/MN103InstructionComparer.cs
@@ -19,7 +19,9 @@
 #endregion
 
 using Reko.Core;
+using Reko.Core.Expressions;
 using Reko.Core.Machine;
+using System;
 
 namespace Reko.Arch.MN103;
 
@@ -31,11 +33,37 @@ public class MN103InstructionComparer : InstructionComparer
 
     public override bool DoCompareOperands(MachineOperand op1, MachineOperand op2)
     {
+        switch (op1)
+        {
+        case RegisterStorage reg1:
+            return base.CompareRegisters(reg1, (RegisterStorage) op2);
+        case Constant imm1:
+            return base.CompareConstants(imm1, (Constant) op2);
+        case Address addr1:
+            return base.CompareAddresses(addr1, (Address) op2);
+        case MemoryOperand mem1:
+            var mem2 = (MemoryOperand) op2;
+            if (!CompareRegisters(mem1.Base, mem2.Base))
+                return false;
+            if (mem1.Displacement != mem2.Displacement)
+                return false;
+            return CompareRegisters(mem1.Index, mem2.Index);
+        }
         throw new System.NotImplementedException();
     }
 
     public override int GetOperandHash(MachineOperand op)
     {
-        throw new System.NotImplementedException();
+        return op switch
+        {
+            RegisterStorage reg => base.GetRegisterHash(reg),
+            Constant imm => base.GetConstantHash(imm),
+            Address addr => base.GetAddressHash(addr),
+            MemoryOperand mem => HashCode.Combine(
+                mem.Displacement,
+                base.GetRegisterHash(mem.Base),
+                base.GetRegisterHash(mem.Index)),
+            _ => throw new System.NotImplementedException()
+        };
     }
 }
